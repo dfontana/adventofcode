8a5efd9566f054f2802943a1720e8705ed439c8f
Dylan Fontana <dyfontana@gmail.com>
2019-12-04 21:26:12 -0500

    Part 2 brute forced

--- 4/ans.go
+++ 4/ans.go
@@ -8,15 +8,15 @@ import (
 func main() {
 	passes := 0
 	for i := 130254; i <= 678275; i++ {
-		if isPassword(int64(i)) {
+		if isPassword(i) {
 			passes++
 		}
 	}
-	fmt.Println("Passes:", passes)
+	fmt.Println("Passes:", passes, isPassword(111111), isPassword(223450), isPassword(123789), isPassword(112233), isPassword(123444), isPassword(111122))
 }
 
-func isPassword(val int64) bool {
-	valStr := strconv.FormatInt(val, 10)
+func isPassword(val int) bool {
+	valStr := strconv.FormatInt(int64(val), 10)
 	var digits []int
 	for _, char := range valStr {
 		digits = append(digits, int(char))
@@ -25,10 +25,26 @@ func isPassword(val int64) bool {
 	prior := -1
 	twoSame := false
 	noIncrease := true
-	for _, digit := range digits {
-		if digit == prior {
+	tooLong := false
+	hasSafeRun := false
+	longestRun := 1
+	for i, digit := range digits {
+		matched := digit == prior
+		if matched {
 			twoSame = true
+			longestRun++
+			if longestRun == 3 {
+				tooLong = true
+			}
 		}
+
+		if !matched || i == len(digits)-1 {
+			if longestRun == 2 {
+				hasSafeRun = true
+			}
+			longestRun = 1
+		}
+
 		if digit < prior {
 			noIncrease = false
 			break
@@ -36,5 +52,5 @@ func isPassword(val int64) bool {
 		prior = digit
 	}
 
-	return twoSame && noIncrease
+	return twoSame && noIncrease && (!tooLong || hasSafeRun)
 }
