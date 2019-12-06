9bfe6700e572fbee694f5cef30b6345686d0403a
Dylan Fontana <dyfontana@gmail.com>
2019-12-05 22:26:54 -0500

    part 1

---
+++ 5/ans.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/dfontana/adventofcode2019/util"
+)
+
+const opCodeAdd = 1
+const opCodeMult = 2
+const opCodeAbort = 99
+const opCodeInput = 3
+const opCodeOuput = 4
+
+func main() {
+	data := parseInput("./input.txt")
+
+	res, _ := run(getMemory(data))
+	fmt.Println("Part 1:", res)
+}
+
+// Blindly takes input from command line and assumes its an integer.
+func getInput() int {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Input: ")
+	text, _ := reader.ReadString('\n')
+	return util.ToInt(text[:len(text)-1])
+}
+
+func getMemory(data []int) []int {
+	memory := make([]int, len(data))
+	copy(memory, data)
+	return memory
+}
+
+func getParamCt(op int) int {
+	switch op {
+	case opCodeAdd:
+	case opCodeMult:
+		return 3
+	case opCodeInput:
+	case opCodeOuput:
+		return 1
+	}
+	return 0
+}
+
+func getOpParams(raw int) (int, []int) {
+	modes := []int{0, 0, 0}
+	op := 0
+	for i := 0; raw > 0; i++ {
+		val := raw % 10
+		switch i {
+		case 0:
+			op += val
+		case 1:
+			op += 10 * val
+		default:
+			modes[i-2] = val
+		}
+		raw /= 10
+	}
+	return op, modes
+}
+
+func applyModes(memory []int, modes []int, params ...int) []int {
+	var items []int
+	for i, param := range params {
+		modeParam := param
+		if modes[i] == 0 {
+			modeParam = memory[param]
+		}
+		items = append(items, modeParam)
+	}
+	return items
+}
+
+func run(memory []int) (int, bool) {
+	ptr := 0
+	maxAddr := len(memory)
+	for {
+		if ptr >= maxAddr {
+			log.Fatal("About to segFault. Aborting...")
+			return 0, true
+		}
+
+		op, modes := getOpParams(memory[ptr])
+		if getParamCt(op) >= maxAddr-ptr {
+			log.Fatal("About to segFault. Aborting...")
+			return 0, true
+		}
+
+		switch op {
+		case opCodeAdd:
+			x, y, ref := memory[ptr+1], memory[ptr+2], memory[ptr+3]
+			params := applyModes(memory, modes, x, y)
+			memory[ref] = params[0] + params[1]
+			ptr += 4
+		case opCodeMult:
+			x, y, ref := memory[ptr+1], memory[ptr+2], memory[ptr+3]
+			params := applyModes(memory, modes, x, y)
+			memory[ref] = params[0] * params[1]
+			ptr += 4
+		case opCodeInput:
+			ref := memory[ptr+1]
+			memory[ref] = getInput()
+			ptr += 2
+		case opCodeOuput:
+			ref := memory[ptr+1]
+			params := applyModes(memory, modes, ref)
+			fmt.Println(params[0])
+			ptr += 2
+		case opCodeAbort:
+			return memory[0], true
+		default:
+			return 0, true
+		}
+	}
+}
+
+func parseInput(filename string) []int {
+	items := util.GetLines(filename)
+	tokens := util.Split(items[0], ",")
+	var result []int
+	for _, token := range tokens {
+		result = append(result, util.ToInt(token))
+	}
+	return result
+}
