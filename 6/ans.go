ce3a769d2986ecd9acaf6490b2e97355cffedc81
Dylan Fontana <dyfontana@gmail.com>
2019-12-09 22:06:38 -0500

    p2

--- 6/ans.go
+++ 6/ans.go
@@ -7,21 +7,59 @@ import (
 
 func main() {
 	edges := parseInput("./input.txt")
-	//edges := parseInput("./test.txt")
 	fmt.Println("P1: ", countDirectAndIndirect(edges))
+	fmt.Println("P2: ", distBetween("YOU", "SAN", edges))
+}
+
+func distBetween(N1 string, N2 string, edges map[string][]string) int {
+	path := make(map[string]int)
+
+	// Walk backwards towards COM from N1, storing distances to each parent
+	object, dist := findOrbit(N1, edges), 0
+	for {
+		path[object] = dist
+		parent := findOrbit(object, edges)
+		if parent == "" {
+			break
+		}
+		object = parent
+		dist++
+	}
+
+	// Walk backwards towards COM from N2, until first intersect occurs
+	object, dist2 := findOrbit(N2, edges), 0
+	for {
+		pathDist, ok := path[object]
+		if ok {
+			dist2 += pathDist
+			break
+		}
+
+		parent := findOrbit(object, edges)
+		if parent == "" {
+			break
+		}
+		object = parent
+		dist2++
+	}
+	return dist2
+}
+
+func findOrbit(node string, edges map[string][]string) string {
+	for parent, neighbors := range edges {
+		if inArray(node, neighbors) {
+			return parent
+		}
+	}
+	return ""
 }
 
 func countDirectAndIndirect(edges map[string][]string) int {
 	ct := 0
 	node := "COM"
 	frontier := []string{node}
-	var visited []string
 	for len(frontier) > 0 {
 		node, frontier = frontier[0], frontier[1:]
-		if inArray(node, visited) {
-			continue
-		}
-		visited = append(visited, node)
 		for _, neighbor := range edges[node] {
 			ct += countNeighbors(neighbor, edges)
 			frontier = append(frontier, neighbor)
@@ -33,13 +71,8 @@ func countDirectAndIndirect(edges map[string][]string) int {
 func countNeighbors(node string, edges map[string][]string) int {
 	ct := 0
 	frontier := []string{node}
-	var visited []string
 	for len(frontier) > 0 {
 		node, frontier = frontier[0], frontier[1:]
-		if inArray(node, visited) {
-			continue
-		}
-		visited = append(visited, node)
 		for _, neighbor := range edges[node] {
 			ct++
 			frontier = append(frontier, neighbor)
