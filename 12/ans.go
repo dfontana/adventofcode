57f3b8cceed35dc5400e75ee78c4247caaae72e9
Dylan Fontana <dyfontana@gmail.com>
2019-12-12 23:12:12 -0500

    Cleanup

--- 12/ans.go
+++ 12/ans.go
@@ -15,14 +15,26 @@ type Vector3 struct {
 	Z int
 }
 
-func (v *Vector3) add(p Vector3) {
-	v.X = p.X + v.X
-	v.Y = p.Y + v.Y
-	v.Z = p.Z + v.Z
+func (v Vector3) get(dim rune) int {
+	switch dim {
+	case 'x':
+		return v.X
+	case 'y':
+		return v.Y
+	default:
+		return v.Z
+	}
 }
 
-func (v Vector3) absSum() int {
-	return int(math.Abs(float64(v.X)) + math.Abs(float64(v.Y)) + math.Abs(float64(v.Z)))
+func (v *Vector3) set(dim rune, val int) {
+	switch dim {
+	case 'x':
+		v.X = val
+	case 'y':
+		v.Y = val
+	case 'z':
+		v.Z = val
+	}
 }
 
 // Moon with Position and Velocity in 3D space
@@ -32,70 +44,38 @@ type Moon struct {
 }
 
 func (m *Moon) accel() {
-	m.P.add(m.V)
+	m.P.X += m.V.X
+	m.P.Y += m.V.Y
+	m.P.Z += m.V.Z
 }
 
-func (m *Moon) gravX(other *Moon) {
-	amount := -1
-	if m.P.X == other.P.X {
+func (m *Moon) grav(dim rune, other *Moon) {
+	if m.P.get(dim) == other.P.get(dim) {
 		return
 	}
-	if m.P.X < other.P.X {
-		amount = 1
-	}
-	m.V.X = m.V.X + amount
-	other.V.X = other.V.X - amount
-}
-
-func (m *Moon) gravY(other *Moon) {
 	amount := -1
-	if m.P.Y == other.P.Y {
-		return
-	}
-	if m.P.Y < other.P.Y {
+	if m.P.get(dim) < other.P.get(dim) {
 		amount = 1
 	}
-	m.V.Y = m.V.Y + amount
-	other.V.Y = other.V.Y - amount
-}
-
-func (m *Moon) gravZ(other *Moon) {
-	amount := -1
-	if m.P.Z == other.P.Z {
-		return
-	}
-	if m.P.Z < other.P.Z {
-		amount = 1
-	}
-	m.V.Z = m.V.Z + amount
-	other.V.Z = other.V.Z - amount
-}
-
-func (m Moon) potential() int {
-	return m.P.absSum()
-}
-
-func (m Moon) kinetic() int {
-	return m.V.absSum()
+	m.V.set(dim, m.V.get(dim)+amount)
+	other.V.set(dim, other.V.get(dim)-amount)
 }
 
 // Moons collection
 type Moons map[int]*Moon
 
-func (ms Moons) applyGravity() {
+func (ms Moons) step() {
 	ct := len(ms)
+	// Apply Gravity
 	for i := 0; i < ct; i++ {
 		for j := i + 1; j < ct; j++ {
-			moon := ms[i]
-			moon2 := ms[j]
-			moon.gravX(moon2)
-			moon.gravY(moon2)
-			moon.gravZ(moon2)
+			ms[i].grav('x', ms[j])
+			ms[i].grav('y', ms[j])
+			ms[i].grav('z', ms[j])
 		}
 	}
-}
 
-func (ms Moons) applyVelocity() {
+	// Then Velocity
 	for _, moon := range ms {
 		moon.accel()
 	}
@@ -103,49 +83,72 @@ func (ms Moons) applyVelocity() {
 
 func (ms Moons) totalEnergy() int {
 	tot := 0
-	for _, moon := range ms {
-		tot += moon.kinetic() * moon.potential()
+	for _, m := range ms {
+		kin := math.Abs(float64(m.V.X)) + math.Abs(float64(m.V.Y)) + math.Abs(float64(m.V.Z))
+		pot := math.Abs(float64(m.P.X)) + math.Abs(float64(m.P.Y)) + math.Abs(float64(m.P.Z))
+		tot += int(kin * pot)
 	}
 	return tot
 }
 
-func (ms Moons) print() {
-	for i, m := range ms {
-		fmt.Println(i, ":", *m)
+func lcm(a, b int) int {
+	x, y := a, b
+	for y != 0 {
+		x, y = y, x%y
 	}
+	return a / x * b
 }
 
 func main() {
 	moons := parseInput("./input.txt")
-	time := 1000
-
-	simulate(moons, time)
-	moons.print()
-	fmt.Println("Total Enegery", moons.totalEnergy())
-}
-
-func simulate(moons Moons, time int) {
-	for i := 0; i < time; i++ {
-		moons.applyGravity()
-		moons.applyVelocity()
+	for i := 0; i < 1000; i++ {
+		moons.step()
 	}
+	fmt.Println("P1 Total Enegery", moons.totalEnergy())
+
+	// All 3 dims do not affect each other. So find the shortest time it takes for each to
+	// independently repeat the first state (period). Then find the least common multiple of all
+	// 3 periods - that's the soonest all 3 will align.
+	moonStart := parseInput("./input.txt")
+	moonSteps := parseInput("./input.txt")
+
+	periodX, periodY, periodZ := 0, 0, 0
+	for step := 1; periodX == 0 || periodY == 0 || periodZ == 0; step++ {
+		moonSteps.step()
+
+		foundX, foundY, foundZ := true, true, true
+		for i, moon := range moonSteps {
+			if moon.P.X != moonStart[i].P.X || moon.V.X != moonStart[i].V.X {
+				foundX = false
+			}
+			if moon.P.Y != moonStart[i].P.Y || moon.V.Y != moonStart[i].V.Y {
+				foundY = false
+			}
+			if moon.P.Z != moonStart[i].P.Z || moon.V.Z != moonStart[i].V.Z {
+				foundZ = false
+			}
+		}
+
+		if foundX && periodX == 0 {
+			periodX = step
+		}
+		if foundY && periodY == 0 {
+			periodY = step
+		}
+		if foundZ && periodZ == 0 {
+			periodZ = step
+		}
+	}
+	fmt.Println("P2", lcm(lcm(periodX, periodY), periodZ))
 }
 
 func parseInput(filename string) Moons {
-	lines := util.GetLines(filename)
 	moons := make(Moons)
 	r := regexp.MustCompile(`-*[0-9]+`)
-	for i, line := range lines {
+	for i, line := range util.GetLines(filename) {
 		matches := r.FindAllString(line, -1)
-		pos := Vector3{
-			X: util.ToInt(matches[0]),
-			Y: util.ToInt(matches[1]),
-			Z: util.ToInt(matches[2]),
-		}
-		moons[i] = &Moon{
-			P: pos,
-			V: Vector3{0, 0, 0},
-		}
+		pos := Vector3{util.ToInt(matches[0]), util.ToInt(matches[1]), util.ToInt(matches[2])}
+		moons[i] = &Moon{pos, Vector3{0, 0, 0}}
 	}
 	return moons
 }
