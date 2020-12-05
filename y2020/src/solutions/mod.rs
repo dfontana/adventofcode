d595046848606adec0c5e3f78667f06aee31ec99
dfontana <dyfontana@gmail.com>
2020-12-04 22:24:41 -0500

    Init 5

--- y2020/src/solutions/mod.rs
+++ y2020/src/solutions/mod.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 use crate::day::{Day, DayArg};
 
@@ -13,6 +14,7 @@ pub fn get_runner(d: DayArg) -> Result<Box<dyn Day>, Box<dyn Error>> {
     DayArg::D(2) => day2::Solve::new(d).map(|s| Box::new(s) as Box<dyn Day>),
     DayArg::D(3) => day3::Solve::new(d).map(|s| Box::new(s) as Box<dyn Day>),
     DayArg::D(4) => day4::Solve::new(d).map(|s| Box::new(s) as Box<dyn Day>),
+    DayArg::D(5) => day5::Solve::new(d).map(|s| Box::new(s) as Box<dyn Day>),
     DayArg::D(n) => Err(format!("Unknown Day Given: {}", n).into()),
   }
 }
