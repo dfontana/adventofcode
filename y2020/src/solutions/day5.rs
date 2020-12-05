b7c8e397f20ecb4469b982c10f0f08918837b7bd
dfontana <dyfontana@gmail.com>
2020-12-05 17:29:33 -0500

    Cleanup second pass

--- y2020/src/solutions/day5.rs
+++ y2020/src/solutions/day5.rs
@@ -1,36 +1,9 @@
 use crate::day::{Day, DayArg};
 use crate::util::read_input;
-use std::{error::Error, ops::RangeInclusive};
+use std::error::Error;
 
 pub struct Solve {
-  seats: Vec<Seat>,
-}
-struct Seat {
-  row_loc: String,
-  col_loc: String,
-  id: i32,
-}
-
-impl Seat {
-  pub fn new(str_split: (&str, &str)) -> Seat {
-    Seat {
-      row_loc: str_split.0.to_string(),
-      col_loc: str_split.1.to_string(),
-      id: 0,
-    }
-  }
-
-  pub fn compute(self) -> Seat {
-    Seat {
-      row_loc: self.row_loc.clone(),
-      col_loc: self.col_loc.clone(),
-      id: search(127, 'F', 'B', 6, &self.row_loc) * 8 + search(7, 'L', 'R', 2, &self.col_loc),
-    }
-  }
-
-  pub fn id(&self) -> i32 {
-    self.id
-  }
+  seats: Vec<i32>,
 }
 
 impl Day for Solve {
@@ -38,10 +11,9 @@ impl Day for Solve {
     let mut seats = read_input(d)?
       .lines()
       .map(|s| (&s[..7], &s[7..]))
-      .map(Seat::new)
-      .map(Seat::compute)
-      .collect::<Vec<Seat>>();
-    seats.sort_by_key(Seat::id);
+      .map(|(row, col)| search(127, &row) * 8 + search(7, &col))
+      .collect::<Vec<i32>>();
+    seats.sort();
     Ok(Solve { seats })
   }
 
@@ -49,7 +21,7 @@ impl Day for Solve {
     self
       .seats
       .last()
-      .map(|s| s.id.to_string())
+      .map(|s| s.to_string())
       .ok_or("No Seats Parsed".into())
   }
 
@@ -57,29 +29,27 @@ impl Day for Solve {
     self
       .seats
       .windows(3)
-      .filter(|win| win[0].id + 1 != win[1].id || win[1].id + 1 != win[2].id)
-      .map(|win| win[1].id + 1)
+      .filter(|win| win[0] + 1 != win[1] || win[1] + 1 != win[2])
       .next()
-      .map(|s| s.to_string())
+      .map(|win| (win[1] + 1).to_string())
       .ok_or("No Missing Seat Found".into())
   }
 }
 
-fn search(max_bound: i32, lower: char, upper: char, stop: usize, loc: &str) -> i32 {
+fn search(max_bound: i32, loc: &str) -> i32 {
   let mut min = 0;
   let mut max = max_bound;
   for (idx, c) in loc.chars().enumerate() {
-    if idx == stop {
+    if idx == loc.len() - 1 {
       return match c {
-        c if c == lower => min,
-        c if c == upper => max,
+        'L' | 'F' => min,
+        'B' | 'R' => max,
         _ => 0,
       };
     }
-
     match c {
-      c if c == lower => max = (max as f32 - ((max - min) as f32 / 2.0)).floor() as i32,
-      c if c == upper => min += ((max - min) as f32 / 2.0).ceil() as i32,
+      'L' | 'F' => max = (max as f32 - ((max - min) as f32 / 2.0)).floor() as i32,
+      'B' | 'R' => min += ((max - min) as f32 / 2.0).ceil() as i32,
       _ => (),
     }
   }
