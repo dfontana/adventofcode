cfef311f83588183be6f326139a9aa9224c8edd6
dfontana <dyfontana@gmail.com>
2020-12-04 20:29:32 -0500

    P1

--- y2020/src/solutions/day4.rs
+++ y2020/src/solutions/day4.rs
@@ -1,20 +1,49 @@
 use crate::day::{Day, DayArg};
 use crate::util::read_input;
 
+use std::collections::HashMap;
+use std::error::Error;
+
+const REQUIRED_KEYS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
 pub struct Day4 {
+  passports: Vec<HashMap<String, String>>,
 }
 
 impl Day for Day4 {
   fn new() -> Result<Day4, Box<dyn Error>> {
-    let input = read_input(DayArg::D(4))?;
-    Ok(Day4 {})
+    let passports = read_input(DayArg::D(4))?
+      .split("\n\n")
+      .map(|passport| {
+        passport
+          .split_whitespace()
+          .map(|token| token.splitn(2, ":"))
+          .map(|mut split| (split.next().unwrap(), split.next().unwrap()))
+          .map(|(k, v)| (k.to_string(), v.to_string()))
+          .collect()
+      })
+      .collect::<Vec<HashMap<String, String>>>();
+    Ok(Day4 { passports })
   }
 
   fn p1(&self) -> Result<String, Box<dyn Error>> {
-    Err("Not implemented".into())
+    Ok(
+      self
+        .passports
+        .iter()
+        .filter(|f| {
+          REQUIRED_KEYS
+            .iter()
+            .filter(|k| !f.contains_key(**k))
+            .count()
+            == 0
+        })
+        .count()
+        .to_string(),
+    )
   }
 
   fn p2(&self) -> Result<String, Box<dyn Error>> {
-    Err("Not implemented".into())
+    Ok("Not implemented".to_string())
   }
 }
