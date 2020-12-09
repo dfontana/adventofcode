f64ab02015ba4a047644a1263a7cf0f32b4e9355
dfontana <dyfontana@gmail.com>
2020-12-08 23:14:31 -0500

    P2

---
+++ y2020/src/solutions/day8.rs
@@ -0,0 +1,114 @@
+use crate::day::{Day, DayArg};
+use crate::util::read_input;
+use std::collections::HashSet;
+use std::error::Error;
+use std::str::FromStr;
+
+pub struct Solve {
+  tape: Vec<Instruction>,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Instruction {
+  NOP(i32),
+  ACC(i32),
+  JMP(i32),
+}
+
+#[derive(Debug)]
+enum Terminate {
+  NORMAL(i32),
+  LOOP(i32),
+  ERROR(String),
+}
+
+impl FromStr for Instruction {
+  type Err = String;
+  fn from_str(input: &str) -> Result<Instruction, Self::Err> {
+    let mut iter = input.splitn(2, " ");
+    let ins = iter
+      .next()
+      .ok_or("Missing Instruction in line")?
+      .to_ascii_lowercase();
+    let amt = iter
+      .next()
+      .and_then(|f| f.parse::<i32>().ok())
+      .ok_or("Missing Argument in line")?;
+    match ins.to_ascii_uppercase().as_str() {
+      "NOP" => Ok(Instruction::NOP(amt)),
+      "ACC" => Ok(Instruction::ACC(amt)),
+      "JMP" => Ok(Instruction::JMP(amt)),
+      _ => Err(format!("Unknown instruction hit: {}", ins)),
+    }
+  }
+}
+
+impl Day for Solve {
+  fn new(d: DayArg) -> Result<Solve, Box<dyn Error>> {
+    Ok(Solve {
+      tape: read_input(d)?
+        .lines()
+        .map(Instruction::from_str)
+        .flatten()
+        .collect(),
+    })
+  }
+
+  fn p1(&self) -> Result<String, Box<dyn Error>> {
+    match run_tape(&self.tape) {
+      Terminate::LOOP(amt) => Ok(amt.to_string()),
+      _ => Err("No loop found".into())
+    }
+  }
+
+  fn p2(&self) -> Result<String, Box<dyn Error>> {
+    let last_accs = self.tape
+      .repeat(self.tape.len())
+      .chunks_mut(self.tape.len())
+      .enumerate()
+      .flat_map(|(idx, chunk)| {
+        let mut jmp = chunk.to_owned();
+        let mut nop = chunk.to_owned();
+        match chunk[idx] {
+          Instruction::NOP(amt) => jmp[idx] = Instruction::JMP(amt),
+          Instruction::JMP(amt) => nop[idx] = Instruction::NOP(amt),
+          _ => return vec![]
+        }
+        vec![(idx, jmp), (idx, nop)]
+      })
+      .map(|(idx, t)| (idx, run_tape(&t.to_vec())))
+      .filter_map(|(idx, res)| {
+        match res {
+          Terminate::NORMAL(amt) => Some((idx, amt)),
+          _ => None,
+        }
+      })
+      .collect::<Vec<(usize, i32)>>();
+    Ok(format!("{:?}", last_accs))
+  }
+}
+
+fn run_tape(tape: &Vec<Instruction>) -> Terminate {
+  let mut seen_idx: HashSet<i32> = HashSet::new();
+  let mut next_idx: i32 = 0;
+  let mut acc = 0;
+  while !seen_idx.contains(&next_idx) {
+    seen_idx.insert(next_idx);
+    if next_idx == tape.len() as i32 {
+      return Terminate::NORMAL(acc);
+    }
+    let ins = match tape.get(next_idx as usize) {
+      None => return Terminate::ERROR("Tape Overflow".to_string()),
+      Some(ins) => ins,
+    };
+    match ins {
+      Instruction::NOP(_) => next_idx += 1,
+      Instruction::ACC(amt) => {
+        acc += amt;
+        next_idx += 1;
+      }
+      Instruction::JMP(amt) => next_idx += amt,
+    }
+  }
+  Terminate::LOOP(acc)
+}
