5a49337ff607401ba2703feea925a77eab2d1f87
dfontana <dyfontana@gmail.com>
2020-12-09 23:05:08 -0500

    Added timer

--- y2020/src/main.rs
+++ y2020/src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate lazy_static;
-
 extern crate dotenv;
 
 mod day;
@@ -8,8 +7,8 @@ mod solutions;
 mod util;
 
 use day::DayArg;
-
 use std::error::Error;
+use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn Error>> {
   dotenv::dotenv().ok();
@@ -18,6 +17,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     .and_then(|v| v.parse::<i32>().ok())
     .ok_or("No Day given to run")?;
   let runner = solutions::get_runner(DayArg::D(day))?;
+
+  let now = Instant::now();
   print!("\n{}\n", runner.run()?);
+  let elapsed = now.elapsed();
+  println!("Elapsed: {:.2?}", elapsed);
   Ok(())
 }
