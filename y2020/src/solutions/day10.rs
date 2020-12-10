de66373124352e39f2827a620f05f90d61e52a67
dfontana <dyfontana@gmail.com>
2020-12-10 12:59:31 -0500

    P1, but in two ways

--- y2020/src/solutions/day10.rs
+++ y2020/src/solutions/day10.rs
@@ -19,24 +19,45 @@ impl Day for Solve {
   }
 
   fn p1(&self) -> Result<String, Box<dyn Error>> {
-    // 336ms
-    let mut copy = self.jolts.clone();
-    copy.sort();
-    copy.push(copy[copy.len() - 1] + 3);
-    copy.insert(0, 0);
-    let counts =
-      copy
-        .windows(2)
-        .map(|win| win[1] - win[0])
-        .fold(HashMap::new(), |mut acc, diff| {
-          acc.entry(diff).and_modify(|v| *v += 1).or_insert(1);
-          acc
-        });
-    let ans = counts.get(&1).unwrap_or(&0) * counts.get(&3).unwrap_or(&0);
-    Ok(ans.to_string())
+    Ok(p1_mut(&self.jolts).to_string())
   }
 
   fn p2(&self) -> Result<String, Box<dyn Error>> {
     Ok("Impl".to_string())
   }
+}
+
+fn p1_imm(jolts: &Vec<i32>) -> i32 {
+  // ~400-600us
+  let mut copy = jolts.clone();
+  copy.sort();
+  copy.push(copy[copy.len() - 1] + 3);
+  copy.insert(0, 0);
+  let counts = copy
+    .windows(2)
+    .map(|win| win[1] - win[0])
+    .fold(HashMap::new(), |mut acc, diff| {
+      acc.entry(diff).and_modify(|v| *v += 1).or_insert(1);
+      acc
+    });
+  counts.get(&1).unwrap_or(&0) * counts.get(&3).unwrap_or(&0)
+}
+
+fn p1_mut(jolts: &Vec<i32>) -> i32 {
+  // ~200-300us
+  let mut copy = jolts.clone();
+  copy.sort();
+  copy.push(copy[copy.len() - 1] + 3);
+  let mut c1 = 0;
+  let mut c3 = 0;
+  let mut prev = 0;
+  for i in copy.iter() {
+    match i - prev {
+      1 => c1 += 1,
+      3 => c3 += 1,
+      _ => (),
+    }
+    prev = *i;
+  }
+  c1 * c3
 }
