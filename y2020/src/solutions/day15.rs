24bd8d96ab7f72cfe7d06bd2b26bafb60259e769
dfontana <dyfontana@gmail.com>
2020-12-15 20:02:12 -0500

    Cleanup

--- y2020/src/solutions/day15.rs
+++ y2020/src/solutions/day15.rs
@@ -19,60 +19,38 @@ impl Day for Solve {
   }
 
   fn p1(&self) -> Result<String, Box<dyn Error>> {
-    let mut ages: HashMap<usize, (usize, usize)> =
-      self
-        .numbers
-        .iter()
-        .enumerate()
-        .fold(HashMap::new(), |mut acc, (t, v)| {
-          acc.insert(*v, (t + 1, t + 1));
-          acc
-        });
-    let mut turn = ages.len() + 1;
-    let mut last_spoken = self.numbers[self.numbers.len() - 1];
-    while turn <= 2020 {
-      last_spoken = match ages.get(&last_spoken) {
-        Some((old, new)) => *new - *old,
-        None => 0,
-      };
-      ages.insert(
-        last_spoken,
-        match ages.get(&last_spoken) {
-          Some((_, new)) => (*new, turn),
-          None => (turn, turn),
-        },
-      );
-      turn += 1;
-    }
-    Ok(last_spoken.to_string())
+    Ok(play_game(2020, &self.numbers).to_string())
   }
 
   fn p2(&self) -> Result<String, Box<dyn Error>> {
-    let mut ages: HashMap<usize, (usize, usize)> =
-      self
-        .numbers
-        .iter()
-        .enumerate()
-        .fold(HashMap::new(), |mut acc, (t, v)| {
-          acc.insert(*v, (t + 1, t + 1));
-          acc
-        });
-    let mut turn = ages.len() + 1;
-    let mut last_spoken = self.numbers[self.numbers.len() - 1];
-    while turn <= 30000000 {
-      last_spoken = match ages.get(&last_spoken) {
-        Some((old, new)) => *new - *old,
-        None => 0,
-      };
-      ages.insert(
-        last_spoken,
-        match ages.get(&last_spoken) {
-          Some((_, new)) => (*new, turn),
-          None => (turn, turn),
-        },
-      );
-      turn += 1;
-    }
-    Ok(last_spoken.to_string())
+    Ok(play_game(30000000, &self.numbers).to_string())
   }
+}
+
+fn play_game(limit: usize, numbers: &Vec<usize>) -> usize {
+  let mut ages: HashMap<usize, (usize, usize)> =
+    numbers
+      .iter()
+      .enumerate()
+      .fold(HashMap::new(), |mut acc, (t, v)| {
+        acc.insert(*v, (t + 1, t + 1));
+        acc
+      });
+  let mut turn = ages.len() + 1;
+  let mut last_spoken = numbers[numbers.len() - 1];
+  while turn <= limit {
+    last_spoken = match ages.get(&last_spoken) {
+      Some((old, new)) => *new - *old,
+      None => 0,
+    };
+    ages.insert(
+      last_spoken,
+      match ages.get(&last_spoken) {
+        Some((_, new)) => (*new, turn),
+        None => (turn, turn),
+      },
+    );
+    turn += 1;
+  }
+  last_spoken
 }
