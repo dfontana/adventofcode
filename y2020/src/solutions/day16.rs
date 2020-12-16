945445fbeede4d706eff24b7c7dbec3029ea33a3
dfontana <dyfontana@gmail.com>
2020-12-16 02:01:07 -0500

    She real ugly, but she work

--- y2020/src/solutions/day16.rs
+++ y2020/src/solutions/day16.rs
@@ -4,7 +4,8 @@ use regex::Regex;
 
 use crate::day::{Day, DayArg};
 use crate::util::read_input;
-use std::{error::Error, ops::RangeInclusive};
+use core::panic;
+use std::{collections::HashSet, error::Error, ops::RangeInclusive};
 
 lazy_static! {
   static ref CAPTURE_RULE: Regex =
@@ -85,6 +86,78 @@ impl Day for Solve {
   }
 
   fn p2(&self) -> Result<String, Box<dyn Error>> {
-    Ok("Impl".to_string())
+    let mut valid_tickets: Vec<Vec<u64>> = self
+      .other_tickets
+      .iter()
+      .filter(|ticket| {
+        ticket
+          .iter()
+          .filter(|v| {
+            !self
+              .rules
+              .iter()
+              .any(|rule| rule.lower.contains(v) || rule.upper.contains(v))
+          })
+          .next()
+          .is_none()
+      })
+      .map(|v| v.clone())
+      .collect();
+    valid_tickets.push(self.my_ticket.clone());
+
+    // Transpose so we look at indicies together
+    let by_idx: Vec<Vec<u64>> = (0..valid_tickets[0].len())
+      .map(|i| {
+        valid_tickets
+          .iter()
+          .map(|inner| inner[i].clone())
+          .collect::<Vec<u64>>()
+      })
+      .collect();
+
+    // Test each index against all rules, assigning rule to index as we find them
+    let mut res: Vec<(Rule, Vec<usize>)> = self
+      .rules
+      .iter()
+      .map(|r| {
+        (
+          r.clone(),
+          by_idx
+            .iter()
+            .enumerate()
+            .filter(|(_, vals)| {
+              vals
+                .iter()
+                .filter(|v| !r.lower.contains(v) && !r.upper.contains(v))
+                .next()
+                .is_none()
+            })
+            .map(|(i, _)| i)
+            .collect::<Vec<usize>>(),
+        )
+      })
+      .collect();
+
+    let mut taken: HashSet<usize> = HashSet::new();
+    let mut assigns: Vec<(Rule, usize)> = Vec::new();
+    res.sort_by(|a, b| a.1.len().cmp(&b.1.len()));
+
+    res.iter().for_each(
+      |(r, idxs)| match idxs.iter().filter(|v| !taken.contains(v)).next() {
+        Some(i) => {
+          assigns.push((r.clone(), *i));
+          taken.insert(*i);
+        }
+        None => panic!("Failed to assign idx"),
+      },
+    );
+
+    let ans = assigns
+      .iter()
+      .filter(|(r, _)| r.name.starts_with("departure"))
+      .map(|(_, i)| self.my_ticket[*i])
+      .product::<u64>();
+
+    Ok(ans.to_string())
   }
 }
