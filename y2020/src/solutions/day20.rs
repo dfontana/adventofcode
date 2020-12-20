7e6cf295d874cabe78cccc8a54cbdf2a7aa8b5f1
dfontana <dyfontana@gmail.com>
2020-12-20 15:45:16 -0500

    parsed

--- y2020/src/solutions/day20.rs
+++ y2020/src/solutions/day20.rs
@@ -2,13 +2,34 @@ use crate::day::{Day, DayArg};
 use crate::util::read_input;
 use std::error::Error;
 
+struct Tile {
+  id: usize,
+  img: Vec<String>,
+}
+
 pub struct Solve {
+  tiles: Vec<Tile>,
 }
 
 impl Day for Solve {
   fn new(d: DayArg) -> Result<Solve, Box<dyn Error>> {
-    let inp = read_input(d)?;
-    Ok(Solve {})
+    Ok(Solve {
+      tiles: read_input(d)?
+        .split("\n\n")
+        .map(|t| {
+          let mut lines = t.lines();
+          let id: usize = lines
+            .next()
+            .unwrap()
+            .trim_start_matches("Tile ")
+            .trim_end_matches(':')
+            .parse()
+            .unwrap();
+          let img = lines.map(str::to_owned).collect();
+          Tile { id, img }
+        })
+        .collect(),
+    })
   }
 
   fn p1(&self) -> Result<String, Box<dyn Error>> {
