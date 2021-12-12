d549ae26beb379725313f9ca134f110b9baea659
Dylan Fontana <dyfontana@gmail.com>
2021-12-12 15:43:38 -0500

    Optimized 12

--- y2021/src/solutions/day12.rs
+++ y2021/src/solutions/day12.rs
@@ -30,14 +30,7 @@ impl Node {
 struct Path {
   top: NodeId,
   up_to: Vec<NodeId>,
-  explored: HashSet<NodeId>,
   used_double: bool,
-}
-
-fn set(v: NodeId) -> HashSet<NodeId> {
-  let mut set = HashSet::new();
-  set.insert(v);
-  set
 }
 
 impl Day for Solve {
@@ -45,132 +38,82 @@ impl Day for Solve {
   where
     Self: Sized,
   {
-    let input = rust_util::read_input(2021, d)?;
-    let mut nodes: Arena = HashMap::new();
-    for line in input.lines() {
-      let mut pair = line.splitn(2, "-");
-      let src = pair.next().unwrap().to_string();
-      let dst = pair.next().unwrap().to_string();
-      // Add the src node + src -> dst conns
-      nodes
-        .entry(src.to_string())
-        .and_modify(|node| node.add(dst.to_string()))
-        .or_insert(Node {
-          id: src.to_string(),
-          edges: set(dst.to_string()),
-          small: src.to_lowercase() == src,
-          terminal: src == "end",
-        });
-
-      // Don't forget to register a dst node (as long as it's not start)
-      nodes
-        .entry(dst.to_string())
-        .and_modify(|node| node.add(src.to_string()))
-        .or_insert(Node {
-          id: dst.to_string(),
-          edges: set(src.to_string()),
-          small: dst.to_lowercase() == dst,
-          terminal: dst == "end",
-        });
-    }
-    Ok(Box::new(Solve { nodes }))
+    Ok(Box::new(Solve {
+      nodes: rust_util::read_input(2021, d)?
+        .lines()
+        .flat_map(|line| {
+          let mut pair = line.splitn(2, "-");
+          let src = pair.next().unwrap();
+          let dst = pair.next().unwrap();
+          vec![(src.clone(), dst.clone()), (dst.clone(), src.clone())]
+        })
+        .fold(HashMap::new(), |mut nodes, (s, d)| {
+          nodes
+            .entry(s.to_string())
+            .and_modify(|node| node.add(d.to_string()))
+            .or_insert(Node {
+              id: s.to_string(),
+              edges: HashSet::from_iter(vec![d.to_string()]),
+              small: s.to_lowercase() == s,
+              terminal: s == "end",
+            });
+          nodes
+        }),
+    }))
   }
 
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    let mut paths: Vec<Path> = Vec::new();
-    let mut frontier: Vec<Path> = self
-      .nodes
-      .get("start")
-      .unwrap()
-      .edges
-      .iter()
-      .map(|e| Path {
-        top: e.clone(),
-        up_to: vec!["start".to_owned()],
-        explored: set("start".into()),
-        used_double: false,
-      })
-      .collect();
-    while let Some(mut crt_path) = frontier.pop() {
-      let next = crt_path.top.clone();
-      let node = self.nodes.get(&next).unwrap();
-      if node.terminal {
-        paths.push(crt_path.clone());
-        continue;
-      }
-
-      // If you are already explored, skip
-      if crt_path.explored.contains(&next) {
-        continue;
-      }
-
-      // Push self to explored, but only if we're small. Everyone else can be visited.
-      if node.small {
-        crt_path.explored.insert(next.clone());
-      }
-
-      // Otherwise push all your edges onto the frontier as new paths
-      node.edges.iter().for_each(|nid| {
-        if nid == "start" {
-          return;
-        }
-        let mut path = crt_path.clone();
-        path.top = nid.clone();
-        path.up_to.push(next.clone());
-        frontier.push(path);
-      });
-    }
-    Ok(Box::new(paths.len()))
+    Ok(Box::new(
+      explore(&self.nodes, &mut init_frontier(&self.nodes, true)).len(),
+    ))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    let mut paths: Vec<Path> = Vec::new();
-    let mut frontier: Vec<Path> = self
-      .nodes
-      .get("start")
-      .unwrap()
+    Ok(Box::new(
+      explore(&self.nodes, &mut init_frontier(&self.nodes, false)).len(),
+    ))
+  }
+}
+
+fn init_frontier(nodes: &Arena, used_double: bool) -> Vec<Path> {
+  nodes
+    .get("start")
+    .unwrap()
+    .edges
+    .iter()
+    .map(|e| Path {
+      top: e.clone(),
+      up_to: vec!["start".to_owned()],
+      used_double,
+    })
+    .collect()
+}
+
+fn explore(nodes: &Arena, frontier: &mut Vec<Path>) -> Vec<Path> {
+  let mut paths: Vec<Path> = Vec::new();
+  while let Some(path) = frontier.pop() {
+    let node = nodes.get(&path.top).unwrap();
+    if node.terminal {
+      paths.push(path.clone());
+      continue;
+    }
+    if node.small && path.up_to.contains(&node.id) && path.used_double {
+      // Small nodes may be visited once or twice, so skip if we have
+      continue;
+    }
+    node
       .edges
       .iter()
-      .map(|e| Path {
-        top: e.clone(),
-        up_to: vec!["start".to_owned()],
-        explored: set("start".into()),
-        used_double: false,
-      })
-      .collect();
-    while let Some(mut crt_path) = frontier.pop() {
-      let next = crt_path.top.clone();
-      let node = self.nodes.get(&next).unwrap();
-      if node.terminal {
-        paths.push(crt_path.clone());
-        continue;
-      }
-
-      // If you are already explored, skip
-      if crt_path.explored.contains(&next) && crt_path.used_double {
-        continue;
-      }
-      let double_used = crt_path.explored.contains(&next) && !crt_path.used_double;
-
-      // Push self to explored, but only if we're small. Everyone else can be visited.
-      if node.small {
-        crt_path.explored.insert(next.clone());
-      }
-
-      // Otherwise push all your edges onto the frontier as new paths
-      node.edges.iter().for_each(|nid| {
-        if nid == "start" {
-          return;
+      .filter(|nid| *nid != "start")
+      .for_each(|nid| {
+        let mut nw_path = path.clone();
+        nw_path.top = nid.clone();
+        nw_path.up_to.push(node.id.clone());
+        if node.small && path.up_to.contains(&node.id) && !path.used_double {
+          nw_path.used_double = true
         }
-        let mut path = crt_path.clone();
-        path.top = nid.clone();
-        path.up_to.push(next.clone());
-        if node.small && double_used {
-          path.used_double = true
-        }
-        frontier.push(path);
+        frontier.push(nw_path);
       });
-    }
-    Ok(Box::new(paths.len()))
   }
+  paths
 }
