12c25d2dabb0b6e7e7574beabaa1ca8178a16dbb
Dylan Fontana <dylan.fontana@datadoghq.com>
2022-12-01 11:07:30 -0500

    Day 1

--- y2022/src/solutions/day1.rs
+++ y2022/src/solutions/day1.rs
@@ -3,7 +3,7 @@ use rust_util::{AocDay, Day};
 use std::{error::Error, fmt::Display};
 
 pub struct Solve {
-  input: String,
+  input: Vec<usize>,
 }
 
 impl Day for Solve {
@@ -11,33 +11,31 @@ impl Day for Solve {
   where
     Self: Sized,
   {
-    let input = rust_util::read_input(2021, d)?;
+    let instr = rust_util::read_input(2022, d)?;
+    let mut input: Vec<usize> = Vec::new();
+    let mut acc: usize = 0;
+    for line in instr.lines() {
+      if line.is_empty() {
+        input.push(acc);
+        acc = 0;
+      } else {
+        acc += line.parse::<usize>().expect("Failed to parse line");
+      }
+    }
+    input.push(acc);
+
     Ok(Box::new(Solve { input }))
   }
 
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
     Ok(Box::new(
-      self
-        .input
-        .lines()
-        .filter_map(|n| n.parse::<usize>().ok())
-        .tuple_windows()
-        .filter(|(a, b)| b > a)
-        .count(),
+      self.input.iter().sorted().rev().take(1).sum::<usize>(),
     ))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
     Ok(Box::new(
-      self
-        .input
-        .lines()
-        .filter_map(|n| n.parse::<usize>().ok())
-        .tuple_windows::<(_, _, _)>()
-        .map(|t| t.0 + t.1 + t.2)
-        .tuple_windows()
-        .filter(|(a, b)| b > a)
-        .count(),
+      self.input.iter().sorted().rev().take(3).sum::<usize>(),
     ))
   }
 }
