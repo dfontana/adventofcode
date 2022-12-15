cf86df0e895bc388805e39b0c442bf827a0287b4
Dylan Fontana <dyfontana@gmail.com>
2022-12-15 17:14:32 -0500

    P1

--- y2022/src/day15.rs
+++ y2022/src/day15.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use rust_util::Day;
 use std::{
   collections::{HashMap, HashSet},
@@ -8,40 +9,77 @@ use std::{
 type Pnt = (i32, i32);
 pub struct Solve {
   pairs: Vec<(Pnt, Pnt)>,
+  row: i32,
+}
+
+#[derive(Eq, PartialEq)]
+enum Spot {
+  Beacon,
+  NotBeacon,
 }
 
 impl TryFrom<String> for Solve {
   type Error = Box<dyn Error>;
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
-    Ok(Solve { pairs: Vec::new() })
+    let regex = Regex::new(
+      "Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)",
+    )
+    .unwrap();
+    Ok(Solve {
+      pairs: value
+        .lines()
+        .map(|l| {
+          let caps = regex.captures(l).unwrap();
+          (
+            (
+              caps.get(1).unwrap().as_str().parse::<i32>().unwrap(),
+              caps.get(2).unwrap().as_str().parse::<i32>().unwrap(),
+            ),
+            (
+              caps.get(3).unwrap().as_str().parse::<i32>().unwrap(),
+              caps.get(4).unwrap().as_str().parse::<i32>().unwrap(),
+            ),
+          )
+        })
+        .collect(),
+      row: 2000000,
+    })
   }
 }
 
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    let mut not_beacons: HashMap<i32, HashSet<Pnt>> = HashMap::new();
-    let mut beacons = HashSet::new();
-    for (sensor, beacon) in self.pairs.iter() { 
-      not_beacons
-        .entry(sensor.1)
-        .and_modify(|v| {
-          v.insert(*sensor);
-        })
-        .or_insert_with(|| {
-          let mut set = HashSet::new();
-          set.insert(*sensor);
-          set
-        });
-      beacons.insert(beacon);
-      // Walk out in all 4 directions the m-distance from sensor,
-      // skipping over beacons
-      let dist = dist_to(sensor, beacon);
+    // X -> Spot, y is assumed = self.row
+    let mut row_comp: HashMap<i32, Spot> = HashMap::new();
+
+    for (sensor, beacon) in self.pairs.iter() {
+      if sensor.1 == self.row {
+        row_comp
+          .entry(sensor.0)
+          .and_modify(|v| *v = Spot::NotBeacon)
+          .or_insert_with(|| Spot::NotBeacon);
+      }
+      region(*sensor, dist_to(sensor, beacon), |(x, y)| {
+        if y == self.row {
+          row_comp
+            .entry(x)
+            .and_modify(|v| *v = Spot::NotBeacon)
+            .or_insert_with(|| Spot::NotBeacon);
+        }
+      });
+
+      if beacon.1 == self.row {
+        row_comp
+          .entry(beacon.0)
+          .and_modify(|v| *v = Spot::Beacon)
+          .or_insert_with(|| Spot::Beacon);
+      }
     }
 
     Ok(Box::new(format!(
       "{:?}",
-      not_beacons.get(&2000000).map(HashSet::len)
+      row_comp.values().filter(|v| **v == Spot::NotBeacon).count()
     )))
   }
 
@@ -50,6 +88,28 @@ impl Day for Solve {
   }
 }
 
-fn dist_to(pntA: &Pnt, pntB: &Pnt) -> i32 {
-  1
+fn dist_to((xa, ya): &Pnt, (xb, yb): &Pnt) -> i32 {
+  (xa - xb).abs() + (ya - yb).abs()
+}
+
+fn region(anchor: Pnt, dist: i32, mut hit: impl FnMut(Pnt)) {
+  // Bottom of triangle
+  let mut width = dist * 2;
+  for row in 0..dist + 1 {
+    for i in 0..width + 1 {
+      let pt = ((anchor.0 - dist + row + i), (anchor.1 + row));
+      hit(pt);
+    }
+    width -= 2;
+  }
+
+  // Top of triangle
+  let mut width = dist * 2;
+  for row in 0..dist + 1 {
+    for i in 0..width + 1 {
+      let pt = ((anchor.0 - dist + row + i), (anchor.1 - row));
+      hit(pt);
+    }
+    width -= 2;
+  }
 }
