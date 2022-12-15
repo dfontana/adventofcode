3f11037839309d55e568d1ed04b2b576890a9a7a
Dylan Fontana <dyfontana@gmail.com>
2022-12-15 18:58:45 -0500

    Working

--- y2022/src/day15.rs
+++ y2022/src/day15.rs
@@ -1,22 +1,12 @@
 use regex::Regex;
 use rust_util::Day;
-use std::{
-  collections::{HashMap, HashSet},
-  error::Error,
-  fmt::Display,
-};
+use std::{error::Error, fmt::Display};
 
-type Pnt = (i32, i32);
+type Pnt = (isize, isize);
 pub struct Solve {
   pairs: Vec<(Pnt, Pnt)>,
-  row: i32,
-  max: i32,
-}
-
-#[derive(Eq, PartialEq)]
-enum Spot {
-  Beacon,
-  NotBeacon,
+  row: isize,
+  max: isize,
 }
 
 impl TryFrom<String> for Solve {
@@ -34,185 +24,102 @@ impl TryFrom<String> for Solve {
           let caps = regex.captures(l).unwrap();
           (
             (
-              caps.get(1).unwrap().as_str().parse::<i32>().unwrap(),
-              caps.get(2).unwrap().as_str().parse::<i32>().unwrap(),
+              caps.get(1).unwrap().as_str().parse().unwrap(),
+              caps.get(2).unwrap().as_str().parse().unwrap(),
             ),
             (
-              caps.get(3).unwrap().as_str().parse::<i32>().unwrap(),
-              caps.get(4).unwrap().as_str().parse::<i32>().unwrap(),
+              caps.get(3).unwrap().as_str().parse().unwrap(),
+              caps.get(4).unwrap().as_str().parse().unwrap(),
             ),
           )
         })
         .collect(),
-      row: 10,
-      max: 20,
+      row: 2000000,
+      max: 4000000,
     })
   }
 }
 
-impl Day for Solve {
-  fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    // X -> Spot, y is assumed = self.row
-    let mut row_comp: HashMap<i32, Spot> = HashMap::new();
+struct Range {
+  pub start: isize,
+  pub end: isize,
+}
 
-    for (sensor, beacon) in self.pairs.iter() {
-      if sensor.1 == self.row {
-        row_comp
-          .entry(sensor.0)
-          .and_modify(|v| *v = Spot::NotBeacon)
-          .or_insert_with(|| Spot::NotBeacon);
-      }
-      region(*sensor, dist_to(sensor, beacon), |(x, y)| {
-        if y == self.row {
-          row_comp
-            .entry(x)
-            .and_modify(|v| *v = Spot::NotBeacon)
-            .or_insert_with(|| Spot::NotBeacon);
-        }
-      });
-      if beacon.1 == self.row {
-        row_comp
-          .entry(beacon.0)
-          .and_modify(|v| *v = Spot::Beacon)
-          .or_insert_with(|| Spot::Beacon);
-      }
-    }
-
-    Ok(Box::new(format!(
-      "{:?}",
-      row_comp.values().filter(|v| **v == Spot::NotBeacon).count()
-    )))
+impl Range {
+  fn intersecting(&self, other: &Range) -> bool {
+    self.end >= other.start && other.end >= self.start
   }
 
-  fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    // Somewhere in (0, 4000000) (for example it's 20)
-    // Ret: x*4000000 + y
-    // There is only 1 undetected spot within those bounds a beacon can be
-    //
-    // Brute force we could go row by row and save all the 'Non' spots
-    // into a hashset, removing any that get spotted in the next row
-    // That would take like 563ms / row * 4,000,000 ~= 26 days
-    //
-    // We can trade more memory by operating on N rows at a time
-    // in memory tho worth of non-spaces. let's say 10000 => 3.7mins
-    let mut row_comp: HashSet<Pnt> = HashSet::new();
+  fn contains(&self, n: isize) -> bool {
+    self.start <= n && n <= self.end
+  }
 
-    let step = 5.clamp(0, self.max);
-    let mut low = 0;
-    let mut high = step;
-    while high <= self.max {
-      let bbox = ((0, low), (self.max, high));
-      println!("Running: {:?}", bbox);
+  fn covering(&self) -> isize {
+    self.end - self.start + 1
+  }
 
-      for (sensor, beacon) in self.pairs.iter() {
-        if in_bbox(bbox, *sensor) {
-          row_comp.insert(*sensor);
-        }
-        region_bbox(bbox, *sensor, dist_to(sensor, beacon), |pt| {
-          row_comp.insert(pt);
-        });
-        if in_bbox(bbox, *beacon) {
-          row_comp.insert(*beacon);
-        }
-      }
-      println!("Done scanning");
-
-      let mut non_spot = None;
-      'outer: for y in low..high + 1 {
-        for x in 0..self.max + 1 {
-          let pt = (x, y);
-          if !row_comp.contains(&pt) {
-            if non_spot.is_none() {
-              non_spot = Some(pt);
-            } else {
-              break 'outer;
-            }
-          }
-        }
-      }
-
-      println!("Done evaluating");
-
-      if let Some((x, y)) = non_spot {
-        // We found something that went undetected in range,
-        // it is the answer. Finish early
-        return Ok(Box::new((x * 4000000 + y).to_string()));
-      }
-      low = high + 1;
-      high += step;
-      row_comp.clear();
+  fn add(self, other: Range) -> Range {
+    Range {
+      start: isize::min(self.start, other.start),
+      end: isize::max(self.end, other.end),
     }
-
-    Err("Not found".into())
   }
 }
 
-fn dist_to((xa, ya): &Pnt, (xb, yb): &Pnt) -> i32 {
+fn dist_to((xa, ya): &Pnt, (xb, yb): &Pnt) -> isize {
   (xa - xb).abs() + (ya - yb).abs()
 }
 
-fn in_bbox((p1, p2): (Pnt, Pnt), (x, y): Pnt) -> bool {
-  x >= p1.0 && x <= p2.0 && y >= p1.1 && y <= p2.1
+fn get_row_coverage(pairs: &Vec<(Pnt, Pnt)>, row: isize) -> Vec<Range> {
+  let mut ranges: Vec<Range> = Vec::new();
+
+  for (sensor, beacon) in pairs {
+    // Get how width the sensor is at this row
+    let mut new_r = match dist_to(sensor, beacon) - (sensor.1 - row).abs() {
+      x if x < 0 => continue,
+      width => Range {
+        start: sensor.0 - width,
+        end: sensor.0 + width,
+      },
+    };
+
+    // Squash down ranges that overlap with this one, so we
+    // have less overall to consider
+    let mut i = 0;
+    while i < ranges.len() {
+      if ranges[i].intersecting(&new_r) {
+        new_r = new_r.add(ranges.remove(i));
+      } else {
+        i += 1;
+      }
+    }
+    ranges.push(new_r);
+  }
+
+  ranges
 }
 
-fn region(anchor: Pnt, dist: i32, mut hit: impl FnMut(Pnt)) {
-  // Bottom of triangle
-  let mut width = dist * 2;
-  for row in 0..dist + 1 {
-    for i in 0..width + 1 {
-      let pt = ((anchor.0 - dist + row + i), (anchor.1 + row));
-      hit(pt);
-    }
-    width -= 2;
+impl Day for Solve {
+  fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
+    let ans = get_row_coverage(&self.pairs, self.row)
+      .iter()
+      .map(|r| r.covering())
+      .sum::<isize>()
+      - 1;
+    Ok(Box::new(format!("{:?}", ans)))
   }
 
-  // Top of triangle
-  let mut width = dist * 2;
-  for row in 0..dist + 1 {
-    for i in 0..width + 1 {
-      let pt = ((anchor.0 - dist + row + i), (anchor.1 - row));
-      hit(pt);
-    }
-    width -= 2;
-  }
-}
-
-fn region_bbox(bbox: (Pnt, Pnt), anchor: Pnt, dist: i32, mut hit: impl FnMut(Pnt)) {
-  // Bottom of triangle
-  let mut width = dist * 2;
-  let mut row = 0;
-  let low_y = anchor.1.clamp(bbox.0 .1, bbox.1 .1);
-  let high_y = (anchor.1 + dist).clamp(bbox.0 .1, bbox.1 .1) + 1;
-  for y in low_y..high_y {
-    let low_x = (anchor.0 - dist + row).clamp(bbox.0 .0, bbox.1 .0);
-    let high_x = (anchor.0 - dist + row + width).clamp(bbox.0 .0, bbox.1 .0) + 1;
-    for i in low_x..high_x {
-      let pt = (i, y);
-      if pt == (14, 11) {
-        println!("Answer hit lower");
-      }
-      hit(pt);
-    }
-    row += 1;
-    width -= 2;
-  }
-
-  // Top of triangle
-  let mut width = dist * 2;
-  let mut row = 0;
-  let low_y = (anchor.1 - dist).clamp(bbox.0 .1, bbox.1 .1);
-  let high_y = (anchor.1).clamp(bbox.0 .1, bbox.1 .1) + 1;
-  for y in low_y..high_y {
-    let low_x = (anchor.0 - dist + row).clamp(bbox.0 .0, bbox.1 .0);
-    let high_x = (anchor.0 - dist + row + width).clamp(bbox.0 .0, bbox.1 .0) + 1;
-    for i in low_x..high_x {
-      let pt = (i, y);
-      if pt == (14, 11) {
-        println!("Answer hit lower");
-      }
-      hit(pt);
-    }
-    row += 1;
-    width -= 2;
+  fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
+    let ans = (0..=self.max)
+      .map(|row| (row, get_row_coverage(&self.pairs, row)))
+      // A row with a gap in it (not 1 big range) has a spot for a beacon
+      .find(|(_, ranges)| ranges.len() > 1)
+      .and_then(|(y, ranges)| {
+        (0..=self.max)
+          // Place where no ranges touch x
+          .find(|x| !ranges.iter().any(|r| r.contains(*x)))
+          .map(|x| x * 4_000_000 + y)
+      });
+    Ok(Box::new(format!("{:?}", ans)))
   }
 }
