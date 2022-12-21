b0c24c1043f9d328eb2104f06a41fe6fa677f054
Dylan Fontana <dyfontana@gmail.com>
2022-12-20 21:56:13 -0500

    P2

--- y2022/src/day20.rs
+++ y2022/src/day20.rs
@@ -1,10 +1,11 @@
 use itertools::Itertools;
 use rust_util::Day;
-use std::{error::Error, fmt::Display, collections::VecDeque};
+use std::{collections::VecDeque, error::Error, fmt::Display};
 
 type Bit = i64;
 pub struct Solve {
   file: VecDeque<(usize, Bit)>,
+  key: Bit,
 }
 
 impl TryFrom<String> for Solve {
@@ -13,10 +14,11 @@ impl TryFrom<String> for Solve {
   fn try_from(value: String) -> Result<Self, Self::Error> {
     Ok(Solve {
       file: value
-    .lines()
-    .filter_map(|v| v.parse::<Bit>().ok())
-    .enumerate()
-    .collect(),
+        .lines()
+        .filter_map(|v| v.parse::<Bit>().ok())
+        .enumerate()
+        .collect(),
+      key: 811589153,
     })
   }
 }
@@ -34,20 +36,34 @@ fn rotate(id: &usize, buffer: &mut VecDeque<(usize, Bit)>) {
   buffer.push_front((id, amt));
 }
 
+fn grove_position(buffer: &VecDeque<(usize, Bit)>) -> Bit {
+  let (offset, _) = buffer.iter().find_position(|(_, v)| v == &0).unwrap();
+  let (_, first) = buffer[(offset + 1000) % buffer.len()];
+  let (_, second) = buffer[(offset + 2000) % buffer.len()];
+  let (_, third) = buffer[(offset + 3000) % buffer.len()];
+  first + second + third
+}
+
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-      let mut buffer = self.file.clone();
+    let mut buffer = self.file.clone();
     for id in 0..buffer.len() {
-        rotate(&id, &mut buffer);
+      rotate(&id, &mut buffer);
     }
-    let (offset, _) = buffer.iter().find_position(|(_, v)| v == &0).unwrap();
-    let (_, first) = buffer[(offset + 1000) % buffer.len()];
-    let (_, second) = buffer[(offset + 2000) % buffer.len()];
-    let (_, third) = buffer[(offset + 3000) % buffer.len()];
-    Ok(Box::new((first+second+third).to_string()))
+    Ok(Box::new(grove_position(&buffer).to_string()))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new("y"))
+    let mut buffer = self
+      .file
+      .iter()
+      .map(|(idx, v)| (*idx, *v * self.key))
+      .collect::<VecDeque<_>>();
+    for _ in 0..10 {
+      for id in 0..buffer.len() {
+        rotate(&id, &mut buffer);
+      }
+    }
+    Ok(Box::new(grove_position(&buffer).to_string()))
   }
 }
