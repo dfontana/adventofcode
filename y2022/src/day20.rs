e91e9ebc2d062349596ed2b85bd049d6e26184e1
Dylan Fontana <dyfontana@gmail.com>
2022-12-20 21:52:28 -0500

    P1

--- y2022/src/day20.rs
+++ y2022/src/day20.rs
@@ -1,9 +1,10 @@
 use itertools::Itertools;
 use rust_util::Day;
-use std::{error::Error, fmt::Display};
+use std::{error::Error, fmt::Display, collections::VecDeque};
 
+type Bit = i64;
 pub struct Solve {
-  file: Vec<(usize, i32)>,
+  file: VecDeque<(usize, Bit)>,
 }
 
 impl TryFrom<String> for Solve {
@@ -11,62 +12,33 @@ impl TryFrom<String> for Solve {
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
     Ok(Solve {
-      file: parse_input(value),
+      file: value
+    .lines()
+    .filter_map(|v| v.parse::<Bit>().ok())
+    .enumerate()
+    .collect(),
     })
   }
 }
 
-fn parse_input(input: String) -> Vec<(usize, i32)> {
-  input
-    .lines()
-    .enumerate()
-    .filter_map(|(idx, v)| v.parse::<i32>().ok().map(|k| (idx, k)))
-    .collect()
-}
-
-fn rotate(id: &usize, buffer: &mut Vec<(usize, i32)>) {
-  // find where the id is
-  let (idx, (_, amt)) = buffer
+fn rotate(id: &usize, buffer: &mut VecDeque<(usize, Bit)>) {
+  let (idx, _) = buffer
     .iter()
     .find_position(|(id_o, _)| id_o == id)
     .expect("Must exist");
 
-  let new_idx = {
-    if amt > &0 {
-      (idx as i32 + amt) as usize % buffer.len()
-    } else {
-       //= 1949
-        // given 0 + -3051 on 5000 buff len
-      let mut new_idx = idx as i32 + amt;
-      if new_idx < 0 {
-        let magnitude = new_idx.abs();
-        let reduced = magnitude % buffer.len() as i32;
-        new_idx = buffer.len() as i32 - reduced;
-        if (idx as i32) < new_idx {
-            // If we remove an element to the left of where this one is going,
-            // the new idx shifts left one. So we need to shift this left one.
-            // Otherwise the shift is "downstream" and doesn't affect this outcome
-            new_idx -= 1;
-        }
-        println!("{} + {} => {} => {} => {}", idx, amt, magnitude, reduced, new_idx);
-        // new_idx = (((idx as i32 + amt).abs() / buffer.len() as i32) + 1) * buffer.len() as i32 + (idx as i32 + amt);
-        //(idx + buffer.len()) as i32 + amt;
-      }
-      new_idx as usize
-    }
-  };
-  // println!("{:?}",buffer);
-  let ele = buffer.remove(idx);
-  
-  buffer.insert(new_idx, ele);
-  
+  buffer.rotate_left(idx); // Bring it to the front
+  let (id, amt) = buffer.pop_front().unwrap();
+  let new_idx = amt.rem_euclid(buffer.len() as Bit) as usize;
+  buffer.rotate_left(new_idx); // Bring the front to the new idx
+  buffer.push_front((id, amt));
 }
-// WRONG: 11212 ; 
+
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    let mut buffer = self.file.clone();
-    for (id, _) in &self.file {
-        rotate(id, &mut buffer);
+      let mut buffer = self.file.clone();
+    for id in 0..buffer.len() {
+        rotate(&id, &mut buffer);
     }
     let (offset, _) = buffer.iter().find_position(|(_, v)| v == &0).unwrap();
     let (_, first) = buffer[(offset + 1000) % buffer.len()];
