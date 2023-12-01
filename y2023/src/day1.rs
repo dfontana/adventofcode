5ee8cb8323cce9d383e18da1d66f9338941dd35d
Dylan Fontana <dyfontana@gmail.com>
2023-11-30 19:22:29 -0500

    Strip impl

--- y2023/src/day1.rs
+++ y2023/src/day1.rs
@@ -10,32 +10,16 @@ impl TryFrom<String> for Solve {
   type Error = Box<dyn Error>;
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
-    let mut input: Vec<usize> = Vec::new();
-    let mut acc: usize = 0;
-    for line in value.lines() {
-      if line.is_empty() {
-        input.push(acc);
-        acc = 0;
-      } else {
-        acc += line.parse::<usize>().expect("Failed to parse line");
-      }
-    }
-    input.push(acc);
-
-    Ok(Solve { input })
+    Ok(Solve { input: vec![] })
   }
 }
 
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(
-      self.input.iter().sorted().rev().take(1).sum::<usize>(),
-    ))
+    Ok(Box::new("p1"))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(
-      self.input.iter().sorted().rev().take(3).sum::<usize>(),
-    ))
+    Ok(Box::new("p2"))
   }
 }
