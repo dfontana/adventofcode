03762c040979a10149a406748407bda85db3c720
Dylan Fontana <dyfontana@gmail.com>
2023-12-04 22:35:41 -0500

    P2

--- y2023/src/day4.rs
+++ y2023/src/day4.rs
@@ -1,7 +1,10 @@
 use itertools::Itertools;
-use regex::Regex;
 use rust_util::Day;
-use std::{collections::HashSet, error::Error, fmt::Display};
+use std::{
+  collections::{HashMap, HashSet},
+  error::Error,
+  fmt::Display,
+};
 
 #[derive(Debug)]
 pub struct Solve {
@@ -10,64 +13,73 @@ pub struct Solve {
 #[derive(Debug)]
 struct Card {
   id: usize,
-  winners: HashSet<usize>,
-  numbers: HashSet<usize>,
-}
-
-impl Card {
-  fn points(&self) -> usize {
-    self
-      .numbers
-      .intersection(&self.winners)
-      .fold(0.5, |acc, _| acc * 2.0) as usize
-  }
+  matching: usize,
 }
 
 impl TryFrom<String> for Solve {
   type Error = Box<dyn Error>;
 
   fn try_from(value: String) -> Result<Self, Self::Error> {
-    let re =
-      Regex::new(r"Card\s+(?<id>[0-9]+):\s+(?<winners>(?:\d+\s*)+)+\|\s+(?<numbers>(?:\d+\s*)+)+")
-        .unwrap();
-
     Ok(Solve {
       cards: value
         .lines()
         .map(|line| {
-          let caps = re.captures(line).unwrap();
-          let id = caps.name("id").unwrap().as_str().parse::<usize>().unwrap();
-          let winners = caps
-            .name("winners")
-            .unwrap()
-            .as_str()
+          let (id, wins, nums) = line
+            .split_once(" | ")
+            .and_then(|(card, nums)| {
+              card
+                .strip_prefix("Card ")
+                .and_then(|c| c.split_once(": "))
+                .map(|(id, wins)| (id, wins, nums))
+            })
+            .unwrap();
+          let winners = wins
             .split_whitespace()
             .map(|n| n.parse::<usize>().unwrap())
-            .collect();
-          let numbers = caps
-            .name("numbers")
-            .unwrap()
-            .as_str()
+            .collect::<HashSet<usize>>();
+          let numbers = nums
             .split_whitespace()
             .map(|n| n.parse::<usize>().unwrap())
-            .collect();
+            .collect::<HashSet<usize>>();
           Card {
-            id,
-            winners,
-            numbers,
+            id: id.trim().parse::<usize>().unwrap(),
+            matching: numbers.intersection(&winners).count(),
           }
         })
-        .collect(),
+        .collect_vec(),
     })
   }
 }
 
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(self.cards.iter().map(Card::points).sum::<usize>()))
+    Ok(Box::new(
+      self
+        .cards
+        .iter()
+        .map(|c| (0..c.matching).fold(0.5, |acc, _| acc * 2.0) as usize)
+        .sum::<usize>(),
+    ))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new("todo"))
+    Ok(Box::new(
+      self
+        .cards
+        .iter()
+        .map(|c| (c.id, c.matching))
+        .fold(HashMap::new(), |mut acc, (id, mc)| {
+          let cards: usize = *acc.entry(id).or_insert(1);
+          for won in 1..=mc {
+            acc
+              .entry(id + won)
+              .and_modify(|v| *v += cards)
+              .or_insert(1 + cards);
+          }
+          acc
+        })
+        .into_values()
+        .sum::<usize>(),
+    ))
   }
 }
