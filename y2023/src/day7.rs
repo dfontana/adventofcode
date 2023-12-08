ccfd76a9edbd613b48a75ebce5ad3cfde46e48e4
Dylan Fontana <dyfontana@gmail.com>
2023-12-07 23:12:36 -0500

    Golfing

--- y2023/src/day7.rs
+++ y2023/src/day7.rs
@@ -1,13 +1,13 @@
 use itertools::Itertools;
 use rust_util::Day;
-use std::{error::Error, fmt::Display};
+use std::{cmp::Ordering, error::Error, fmt::Display};
 
 #[derive(Debug)]
 pub struct Solve {
   input: Vec<(String, String)>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Ord)]
 struct Hand {
   typ: HandType,
   cards: [Card; 5],
@@ -43,6 +43,44 @@ enum HandType {
   Five,
 }
 
+impl PartialEq for Hand {
+  fn eq(&self, other: &Self) -> bool {
+    for i in 0..=4 {
+      if self.cards[i] != other.cards[i] {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+impl PartialOrd for Hand {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    match self.typ.cmp(&other.typ) {
+      Ordering::Equal => (0..=4)
+        .map(|i| self.cards[i].cmp(&other.cards[i]))
+        .filter(|cmp| *cmp != Ordering::Equal)
+        .next()
+        .or(Some(Ordering::Equal)),
+      c => Some(c),
+    }
+  }
+}
+
+impl TryFrom<String> for Solve {
+  type Error = Box<dyn Error>;
+
+  fn try_from(value: String) -> Result<Self, Self::Error> {
+    Ok(Solve {
+      input: value
+        .lines()
+        .filter_map(|s| s.split_once(" "))
+        .map(|(h, b)| (h.to_string(), b.to_string()))
+        .collect(),
+    })
+  }
+}
+
 impl From<&[Card; 5]> for HandType {
   fn from(value: &[Card; 5]) -> Self {
     let mut cards = value.clone();
@@ -56,17 +94,12 @@ impl From<&[Card; 5]> for HandType {
         jokers += 1;
         continue;
       }
-      match last_seen {
-        None => compressed[i] += 1,
-        Some(c) => {
-          if c == card {
-            compressed[i] += 1;
-          } else {
-            i += 1;
-            compressed[i] += 1;
-          }
+      if let Some(c) = last_seen {
+        if c != card {
+          i += 1;
         }
       }
+      compressed[i] += 1;
       last_seen = Some(card);
     }
     compressed.sort();
@@ -94,113 +127,52 @@ impl From<&[Card; 5]> for HandType {
   }
 }
 
-impl Eq for Hand {}
-impl PartialEq for Hand {
-  fn eq(&self, other: &Self) -> bool {
-    for i in 0..=4 {
-      if self.cards[i] != other.cards[i] {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-impl PartialOrd for Hand {
-  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-    if self.typ > other.typ {
-      return Some(std::cmp::Ordering::Greater);
-    } else if self.typ < other.typ {
-      return Some(std::cmp::Ordering::Less);
-    }
-
-    for i in 0..=4 {
-      if self.cards[i] > other.cards[i] {
-        return Some(std::cmp::Ordering::Greater);
-      } else if self.cards[i] < other.cards[i] {
-        return Some(std::cmp::Ordering::Less);
-      }
-    }
-    Some(std::cmp::Ordering::Equal)
-  }
-}
-
-impl Ord for Hand {
-  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-    self.partial_cmp(other).unwrap()
-  }
-}
-
-impl TryFrom<String> for Solve {
-  type Error = Box<dyn Error>;
-
-  fn try_from(value: String) -> Result<Self, Self::Error> {
-    Ok(Solve {
-      input: value
-        .lines()
-        .filter_map(|s| s.split_once(" "))
-        .map(|(h, b)| (h.to_string(), b.to_string()))
-        .collect(),
-    })
-  }
-}
-impl TryFrom<(&(String, String), bool)> for Hand {
-  type Error = Box<dyn Error>;
-
-  fn try_from(((hand, bid), is_jk): (&(String, String), bool)) -> Result<Self, Self::Error> {
-    let c = hand
-      .chars()
-      .map(|c| match c {
-        '2' => Card::N2,
-        '3' => Card::N3,
-        '4' => Card::N4,
-        '5' => Card::N5,
-        '6' => Card::N6,
-        '7' => Card::N7,
-        '8' => Card::N8,
-        '9' => Card::N9,
-        'T' => Card::T,
-        'J' if !is_jk => Card::J,
-        'J' if is_jk => Card::JK,
-        'Q' => Card::Q,
-        'K' => Card::K,
-        'A' => Card::A,
-        _ => unreachable!(),
+impl Solve {
+  fn total_winnings(&self, with_jokers: bool) -> usize {
+    self
+      .input
+      .iter()
+      .map(|(hand, bid)| {
+        let c = hand
+          .chars()
+          .map(|c| match c {
+            '2' => Card::N2,
+            '3' => Card::N3,
+            '4' => Card::N4,
+            '5' => Card::N5,
+            '6' => Card::N6,
+            '7' => Card::N7,
+            '8' => Card::N8,
+            '9' => Card::N9,
+            'T' => Card::T,
+            'J' if !with_jokers => Card::J,
+            'J' if with_jokers => Card::JK,
+            'Q' => Card::Q,
+            'K' => Card::K,
+            'A' => Card::A,
+            _ => unreachable!(),
+          })
+          .collect_vec();
+        let cards = [c[0], c[1], c[2], c[3], c[4]];
+        Hand {
+          typ: HandType::from(&cards),
+          cards,
+          bid: bid.parse().unwrap(),
+        }
       })
-      .collect_vec();
-    let cards = [c[0], c[1], c[2], c[3], c[4]];
-    Ok(Hand {
-      typ: HandType::from(&cards),
-      cards,
-      bid: bid.parse()?,
-    })
+      .sorted()
+      .enumerate()
+      .map(|(rank, hand)| (rank + 1) * hand.bid)
+      .sum::<usize>()
   }
 }
 
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(
-      self
-        .input
-        .iter()
-        .filter_map(|s| Hand::try_from((s, false)).ok())
-        .sorted()
-        .enumerate()
-        .map(|(rank, hand)| (rank + 1) * hand.bid)
-        .sum::<usize>(),
-    ))
+    Ok(Box::new(self.total_winnings(false)))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(
-      self
-        .input
-        .iter()
-        .filter_map(|s| Hand::try_from((s, true)).ok())
-        .sorted()
-        .enumerate()
-        .map(|(rank, hand)| (rank + 1) * hand.bid)
-        .sum::<usize>(),
-    ))
+    Ok(Box::new(self.total_winnings(true)))
   }
 }
