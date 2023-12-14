a80f137ff15b72a339ee5d26e08cb4afd8ea35a8
Dylan Fontana <dyfontana@gmail.com>
2023-12-13 22:07:10 -0500

    P1 & P2

--- y2023/src/day12.rs
+++ y2023/src/day12.rs
@@ -1,13 +1,12 @@
 use rust_util::Day;
-use std::{error::Error, fmt::Display};
-use tracing::debug;
+use std::{collections::HashMap, error::Error, fmt::Display};
 
 #[derive(Debug)]
 pub struct Solve {
   input: Vec<(Vec<SpaState>, Vec<usize>)>,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
 enum SpaState {
   OK,
   BR,
@@ -46,150 +45,101 @@ impl TryFrom<String> for Solve {
   }
 }
 
-fn combinations((spas, grps): &(Vec<SpaState>, Vec<usize>)) -> usize {
-  let num_grps = grps.len();
-  let num_spas = spas.len();
+fn combos(
+  spas: Vec<SpaState>,
+  grps: Vec<usize>,
+  cache: &mut HashMap<(Vec<SpaState>, Vec<usize>), usize>,
+) -> usize {
+  let key = (spas.clone(), grps.clone());
+  if let Some(v) = cache.get(&key) {
+    return *v;
+  }
+  let mut nspa = spas.clone();
+  let mut i = 0;
+  while i < nspa.len() {
+    if nspa[i] == SpaState::OK {
+      i += 1;
+    } else {
+      break;
+    }
+  }
+  nspa = nspa[i..].to_vec();
 
-  let mut combos = 0;
+  if nspa.is_empty() {
+    let ret = if grps.is_empty() { 1 } else { 0 };
+    cache.insert(key, ret);
+    return ret;
+  }
 
-  let mut frontier = vec![(spas.clone(), 0, 0)];
-  while let Some((spa, mut offset, grp_idx)) = frontier.pop() {
-    // Layout logic
-    let grp = grps[grp_idx];
-    debug!("0: POP {:?} <- {:?} ({:?})", disp(&spa), grp, offset);
-    'outer: while offset < num_spas && offset + grp <= num_spas {
-      let mut nspa = spa.clone();
+  if grps.is_empty() {
+    let ret = if !nspa.contains(&SpaState::BR) { 1 } else { 0 };
+    cache.insert(key, ret);
+    return ret;
+  }
 
-      debug!("\ta: {:?} <- {:?} ({:?})", disp(&nspa), grp, offset);
-
-      for i in offset..offset + grp {
-        if nspa[i] == SpaState::OK {
-          // This layout is no good, it bisects '.'
-          debug!("\tf: {:?} <> {:?}", disp(&nspa), i);
-          offset += 1;
-          continue 'outer;
-        }
-        nspa[i] = SpaState::BR;
-      }
-
-      debug!("\tb: {:?}", disp(&nspa));
-
-      if grp_idx != num_grps - 1 && offset + grp + 1 >= num_spas {
-        // This layout is no good, more groups to go and not enough space to add another
-        debug!(
-          "\tc: {:?}!={:?} && {:?}+{:?}+2 >= {:?}",
-          grp_idx, num_grps, offset, grp, num_spas
-        );
-        offset += 1;
-        continue;
-      }
-
-      if nspa
-        .get(offset + grp)
-        .filter(|c| **c == SpaState::BR)
-        .is_some()
-        || offset
-          .checked_sub(1)
-          .and_then(|c| nspa.get(c))
-          .filter(|c| **c == SpaState::BR)
-          .is_some()
-      {
-        debug!("\td: {:?} == #", offset + grp);
-        // This layout is no good, as we've made too long a chain
-        offset += 1;
-        continue;
-      }
-
-      if grp_idx >= num_grps - 1 {
-        debug!("\tVALID {:?}", disp(&nspa));
-        combos += 1;
-      } else {
-        debug!(
-          "   0: PUSH {:?} <- {:?} ({:?})",
-          disp(&nspa),
-          grps[grp_idx + 1],
-          offset + grp + 1
-        );
-        frontier.push((nspa, offset + grp + 1, grp_idx + 1))
-      }
-
-      offset += 1;
+  if nspa[0] == SpaState::BR {
+    if nspa.len() < grps[0] || nspa[..grps[0]].contains(&SpaState::OK) {
+      cache.insert(key, 0);
+      return 0;
+    } else if nspa.len() == grps[0] {
+      let ret = if grps.len() == 1 { 1 } else { 0 };
+      cache.insert(key, ret);
+      return ret;
+    } else if nspa[grps[0]] == SpaState::BR {
+      cache.insert(key, 0);
+      return 0;
+    } else {
+      let ret = combos(nspa[grps[0] + 1..].to_vec(), grps[1..].to_vec(), cache);
+      cache.insert(key, ret);
+      return ret;
     }
   }
 
-  // attempt to lay down the first group, if "valid" push to "frontier"
-  // "valid":
-  //    1) must not bisect any '.'
-  //    2) if more there's another group left, must have '.' at end
-  //    3) must be the exact right size
-  // increase "offset" by 1 & repeat above
-  // stop when offset == vec len
-
-  // then move to next item in group and start from 1 space after end
-  combos
+  let mut updated = nspa[1..].to_vec();
+  updated.insert(0, SpaState::BR);
+  let ret = combos(updated, grps.clone(), cache) + combos(nspa[1..].to_vec(), grps, cache);
+  cache.insert(key, ret);
+  return ret;
 }
 
-fn disp(spas: &Vec<SpaState>) -> String {
-  let mut s = String::new();
-  for sa in spas {
-    let p = match sa {
-      SpaState::OK => '.',
-      SpaState::BR => '#',
-      SpaState::UK => '?',
-    };
-    s.push(p);
+fn five_x((spas, grps): &(Vec<SpaState>, Vec<usize>)) -> (Vec<SpaState>, Vec<usize>) {
+  let mut nspa = Vec::new();
+  let mut ngrp = Vec::new();
+  for i in 1..=5 {
+    for s in spas.iter() {
+      nspa.push(s.clone());
+    }
+    for g in grps.iter() {
+      ngrp.push(*g);
+    }
+    if i != 5 {
+      nspa.push(SpaState::UK);
+    }
   }
-  s
+  (nspa, ngrp)
 }
 
 impl Day for Solve {
   fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(self.input.iter().map(combinations).sum::<usize>()))
+    let mut cache = HashMap::new();
+    Ok(Box::new(
+      self
+        .input
+        .iter()
+        .map(|(s, g)| combos(s.clone(), g.clone(), &mut cache))
+        .sum::<usize>(),
+    ))
   }
 
   fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-    Ok(Box::new(1))
-  }
-}
-
-#[cfg(test)]
-mod test {
-  use super::*;
-  use tracing_test::traced_test;
-
-  #[traced_test]
-  #[test]
-  fn case1() {
-    assert_eq!(combinations(&parse_spa("???.### 1,1,3")), 1);
-  }
-
-  #[traced_test]
-  #[test]
-  fn case2() {
-    assert_eq!(combinations(&parse_spa(".??..??...?##. 1,1,3")), 4);
-  }
-
-  #[traced_test]
-  #[test]
-  fn case3() {
-    assert_eq!(combinations(&parse_spa("?#?#?#?#?#?#?#? 1,3,1,6")), 1);
-  }
-
-  #[traced_test]
-  #[test]
-  fn case4() {
-    assert_eq!(combinations(&parse_spa("????.#...#... 4,1,1")), 1);
-  }
-
-  #[traced_test]
-  #[test]
-  fn case5() {
-    assert_eq!(combinations(&parse_spa("????.######..#####. 1,6,5")), 4);
-  }
-
-  #[traced_test]
-  #[test]
-  fn case6() {
-    assert_eq!(combinations(&parse_spa("?###???????? 3,2,1")), 10);
+    let mut cache = HashMap::new();
+    Ok(Box::new(
+      self
+        .input
+        .iter()
+        .map(five_x)
+        .map(|(s, g)| combos(s.clone(), g.clone(), &mut cache))
+        .sum::<usize>(),
+    ))
   }
 }
