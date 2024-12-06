02a750a353365cd412ae63e81bafff8da9ad3e79
Dylan Fontana <dyfontana@gmail.com>
2024-12-06 18:13:08 -0500

    P1

--- y2024/src/day6.rs
+++ y2024/src/day6.rs
@@ -1,5 +1,9 @@
+use itertools::Itertools;
 use rust_util::Day;
-use std::{collections::HashMap, error::Error};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+};
 
 pub struct Solve {
     by_x: HashMap<usize, Vec<usize>>,
@@ -22,7 +26,7 @@ impl TryFrom<String> for Solve {
     fn try_from(value: String) -> Result<Self, Self::Error> {
         let mut lines = value.lines();
         let width = lines.nth(0).map(|v| v.len()).unwrap();
-        let height = lines.count() + 1;
+        let height = lines.count();
         let mut by_x = HashMap::new();
         let mut by_y = HashMap::new();
         let mut patrol = (0, 0, Dir::N);
@@ -64,9 +68,10 @@ impl TryFrom<String> for Solve {
 }
 
 impl Day for Solve {
+    // 229 -> Too low
     fn p1(&self) -> Result<Box<dyn std::fmt::Display>, Box<dyn std::error::Error>> {
         let (mut px, mut py, mut pd) = self.patrol.clone();
-        let mut coords: Vec<(usize, usize)> = vec![(px, py)];
+        let mut coords: Vec<(usize, usize, Dir)> = vec![(px, py, pd.clone())];
         loop {
             let next_spot = match pd {
                 Dir::N => self
@@ -77,12 +82,12 @@ impl Day for Solve {
                 Dir::S => self
                     .by_x
                     .get(&px)
-                    .and_then(|v| find_highest_gt(py, v))
+                    .and_then(|v| find_lowest_gt(py, v))
                     .map(|ny| (px, ny)),
                 Dir::E => self
                     .by_y
                     .get(&py)
-                    .and_then(|v| find_highest_gt(px, v))
+                    .and_then(|v| find_lowest_gt(px, v))
                     .map(|nx| (nx, py)),
                 Dir::W => self
                     .by_y
@@ -93,15 +98,15 @@ impl Day for Solve {
             if next_spot.is_none() {
                 // Off the board, clamp to bounds and break
                 coords.push(match pd {
-                    Dir::N => (px, 0),
-                    Dir::S => (px, self.height),
-                    Dir::E => (self.width, py),
-                    Dir::W => (0, py),
+                    Dir::N => (px, 0, pd),
+                    Dir::S => (px, self.height, pd),
+                    Dir::E => (self.width, py, pd),
+                    Dir::W => (0, py, pd),
                 });
                 break;
             }
             let (nx, ny) = next_spot.unwrap();
-            coords.push((nx, ny));
+            coords.push((nx, ny, pd.clone()));
             // Otherwise turn right from the new spot
             pd = match pd {
                 Dir::N => Dir::E,
@@ -112,7 +117,7 @@ impl Day for Solve {
             px = nx;
             py = ny;
         }
-        println!("{:?}", coords);
+        // debug(&coords, self);
         Ok(Box::new(count_distinct_path(&coords)))
     }
 
@@ -121,13 +126,52 @@ impl Day for Solve {
     }
 }
 
-fn count_distinct_path(coords: &[(usize, usize)]) -> usize {
-    // Walk the list of coordinates and count each unique (x,y) visited
-    todo!()
+fn debug(coords: &[(usize, usize, Dir)], solve: &Solve) {
+    let path = coords
+        .iter()
+        .tuple_windows()
+        .flat_map(|(c1, c2)| walk(c1, c2))
+        .collect::<HashSet<_>>();
+    for (y, v) in vec![vec!['.'; solve.width]; solve.height + 1]
+        .iter()
+        .enumerate()
+    {
+        for (x, i) in v.iter().enumerate() {
+            if solve.patrol.0 == x && solve.patrol.1 == y {
+                print!("^");
+            } else if solve.by_y.get(&y).filter(|v| v.contains(&x)).is_some() {
+                print!("#");
+            } else if path.contains(&(x, y)) {
+                print!("X");
+            } else {
+                print!("{}", i);
+            }
+        }
+        println!();
+    }
 }
 
-fn find_highest_gt(px: usize, v: &[usize]) -> Option<usize> {
-    v.iter().filter(|i| **i > px).max().map(|i| i - 1)
+fn count_distinct_path(coords: &[(usize, usize, Dir)]) -> usize {
+    // Walk the list of coordinates and count each unique (x,y) visited
+    coords
+        .iter()
+        .tuple_windows()
+        .flat_map(|(c1, c2)| walk(c1, c2))
+        .collect::<HashSet<_>>()
+        .len()
+}
+
+fn walk(c1: &(usize, usize, Dir), c2: &(usize, usize, Dir)) -> Vec<(usize, usize)> {
+    match c2.2 {
+        Dir::N => (c2.1..=c1.1).map(|y| (c1.0, y)).collect(),
+        Dir::S => (c1.1..=c2.1).map(|y| (c1.0, y)).collect(),
+        Dir::E => (c1.0..=c2.0).map(|x| (x, c1.1)).collect(),
+        Dir::W => (c2.0..=c1.0).map(|x| (x, c1.1)).collect(),
+    }
+}
+
+fn find_lowest_gt(px: usize, v: &[usize]) -> Option<usize> {
+    v.iter().filter(|i| **i > px).min().map(|i| i - 1)
 }
 
 fn find_highest_lt(px: usize, v: &[usize]) -> Option<usize> {
@@ -153,6 +197,12 @@ mod test {
             .to_string();
 
         let solve = Solve::try_from(input).unwrap();
-        solve.p1().unwrap();
+        assert_eq!(
+            solve
+                .p1()
+                .map(|v| format!("{}", v).parse::<usize>().unwrap())
+                .unwrap(),
+            41
+        );
     }
 }
