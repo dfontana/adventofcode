273a970b2e26e90be9cf4d2c4e72b26eab183822
Dylan Fontana <dyfontana@gmail.com>
2024-12-06 23:55:50 -0500

    P2

--- y2024/src/day6.rs
+++ y2024/src/day6.rs
@@ -37,15 +37,6 @@ impl TryFrom<String> for Solve {
                     '^' => {
                         patrol = (x, y, Dir::N);
                     }
-                    '>' => {
-                        patrol = (x, y, Dir::E);
-                    }
-                    'V' => {
-                        patrol = (x, y, Dir::S);
-                    }
-                    '<' => {
-                        patrol = (x, y, Dir::W);
-                    }
                     '#' => {
                         by_x.entry(x)
                             .and_modify(|v: &mut Vec<_>| v.push(y))
@@ -71,18 +62,52 @@ impl TryFrom<String> for Solve {
 impl Day for Solve {
     fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
         Ok(Box::new(
-            self.get_turns()
-                .iter()
-                .tuple_windows()
-                .flat_map(|(c1, c2)| walk(c1, c2))
-                .map(|(a, b, _)| (a, b))
-                .collect::<HashSet<_>>()
-                .len(),
+            get_turns(
+                &self.patrol,
+                &self.by_x,
+                &self.by_y,
+                self.width,
+                self.height,
+            )
+            .unwrap()
+            .iter()
+            .tuple_windows()
+            .flat_map(|(c1, c2)| walk(c1, c2))
+            .map(|(a, b, _)| (a, b))
+            .collect::<HashSet<_>>()
+            .len(),
         ))
     }
 
     fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-        Ok(Box::new(self.p2s()))
+        Ok(Box::new(
+            // Walk the path, at each step add a block in all 3 directions and finish the walk (test for loop).
+            // Keep those that cause loop.
+            // I have a bug and am too tired to know why. But for some reason the code
+            // tries to put one in front of the guard and thinks it's valid. Let's remove it.
+            get_turns(
+                &self.patrol,
+                &self.by_x,
+                &self.by_y,
+                self.width,
+                self.height,
+            )
+            .unwrap()
+            .iter()
+            .tuple_windows()
+            .flat_map(|(c1, c2)| walk(c1, c2))
+            .flat_map(|(x, y, d)| match d {
+                // Pair point with with each neighbor
+                Dir::N => vec![(x - 1, y), (x + 1, y), (x, y - 1)],
+                Dir::S => vec![(x - 1, y), (x + 1, y), (x, y + 1)],
+                Dir::E => vec![(x, y - 1), (x, y + 1), (x + 1, y)],
+                Dir::W => vec![(x, y - 1), (x, y + 1), (x - 1, y)],
+            })
+            .filter(|test| self.triggers_loop(test))
+            .collect::<HashSet<_>>()
+            .len()
+                - 1,
+        ))
     }
 }
 
@@ -133,25 +158,37 @@ impl Dir {
     }
 }
 
-impl Solve {
-    fn get_turns(&self) -> Vec<(usize, usize, Dir)> {
-        let (mut px, mut py, mut pd) = self.patrol.clone();
-        let mut coords: Vec<(usize, usize, Dir)> = vec![(px, py, pd.clone())];
-        loop {
-            let next_spot = pd.next(&self.by_x, &self.by_y, px, py);
-            if next_spot.is_none() {
-                coords.push(pd.last(self.width, self.height, px, py));
-                break;
-            }
-            let (nx, ny) = next_spot.unwrap();
-            coords.push((nx, ny, pd.clone()));
-            pd = pd.turn();
-            px = nx;
-            py = ny;
+fn get_turns(
+    patrol: &(usize, usize, Dir),
+    by_x: &HashMap<usize, Vec<usize>>,
+    by_y: &HashMap<usize, Vec<usize>>,
+    width: usize,
+    height: usize,
+) -> Option<Vec<(usize, usize, Dir)>> {
+    let (mut px, mut py, mut pd) = patrol.clone();
+    let mut coords: Vec<(usize, usize, Dir)> = vec![(px, py, pd.clone())];
+    let mut seen: HashSet<(usize, usize, Dir)> = HashSet::from_iter(coords.clone());
+    loop {
+        let next_spot = pd.next(&by_x, &by_y, px, py);
+        if next_spot.is_none() {
+            coords.push(pd.last(width, height, px, py));
+            break;
         }
-        coords
+        let (nx, ny) = next_spot.unwrap();
+        let nxt_pt = (nx, ny, pd.clone());
+        if seen.contains(&nxt_pt) {
+            return None;
+        }
+        coords.push(nxt_pt.clone());
+        seen.insert(nxt_pt);
+        pd = pd.turn();
+        px = nx;
+        py = ny;
     }
+    Some(coords)
+}
 
+impl Solve {
     fn triggers_loop(&self, item: &(usize, usize)) -> bool {
         // Insert item into by_x & by_y and see if you hit a seen known during loop
         let mut by_x = self.by_x.clone();
@@ -162,109 +199,8 @@ impl Solve {
         by_y.entry(item.1)
             .and_modify(|v| v.push(item.0))
             .or_insert_with(|| vec![item.0]);
-        let (mut px, mut py, mut pd) = self.patrol.clone();
-        let mut coords: HashSet<(usize, usize, Dir)> =
-            HashSet::from_iter(vec![(px, py, pd.clone())]);
-        loop {
-            let next_spot = pd.next(&by_x, &by_y, px, py);
-            if next_spot.is_none() {
-                return false;
-            }
-            let (nx, ny) = next_spot.unwrap();
-            let nxt_pt = (nx, ny, pd.clone());
-            if coords.contains(&nxt_pt) {
-                return true;
-            }
-            coords.insert(nxt_pt);
-            pd = pd.turn();
-            px = nx;
-            py = ny;
-        }
+        get_turns(&self.patrol, &by_x, &by_y, self.width, self.height).is_none()
     }
-
-    // 792 -> too low
-    fn p2s(&self) -> usize {
-        let turns = self.get_turns();
-        let turnset: HashSet<(usize, usize, Dir)> = HashSet::from_iter(turns.clone());
-        // So if we put an object that causes us to return to the a spot in "turns" then we've made a loop
-        // Naive option would be to walk the entire puzzle and at each step place a obstacle to see if it
-        // hits another turn.
-
-        // Better - take the paths walked; and every other "turn" point. See if any point on this path can make a perpendicular line to the other turn, such that it has the same origin direction.
-        let items = turns
-            .iter()
-            .tuple_windows()
-            .flat_map(|(t1, t2)| walk(t1, t2))
-            .filter_map(|point| try_line(point, &turnset))
-            .collect::<HashSet<_>>();
-        // Filter items that don't loop but instead exit
-        let items = items
-            .iter()
-            .filter(|item| self.triggers_loop(*item))
-            .cloned()
-            .collect::<HashSet<_>>();
-        debug(&self.get_turns(), &items, self);
-        items.len()
-    }
-}
-
-fn debug(coords: &[(usize, usize, Dir)], itemset: &HashSet<(usize, usize)>, solve: &Solve) {
-    let coordset = coords
-        .iter()
-        .map(|(a, b, _)| (a, b))
-        .collect::<HashSet<_>>();
-    let path = coords
-        .iter()
-        .tuple_windows()
-        .flat_map(|(c1, c2)| walk(c1, c2))
-        .map(|(a, b, _)| (a, b))
-        .collect::<HashSet<_>>();
-    for (y, v) in vec![vec!['.'; solve.width]; solve.height + 1]
-        .iter()
-        .enumerate()
-    {
-        for (x, i) in v.iter().enumerate() {
-            if solve.patrol.0 == x && solve.patrol.1 == y {
-                print!("^");
-            } else if solve.by_y.get(&y).filter(|v| v.contains(&x)).is_some() {
-                print!("#");
-            } else if itemset.contains(&(x, y)) {
-                print!("O");
-            } else if coordset.contains(&(&x, &y)) {
-                print!("+");
-            } else if path.contains(&(x, y)) {
-                print!(".");
-            } else {
-                print!("{}", i);
-            }
-        }
-        println!();
-    }
-}
-
-fn try_line(
-    point: (usize, usize, Dir),
-    turns: &HashSet<(usize, usize, Dir)>,
-) -> Option<(usize, usize)> {
-    // Can point form a perpendicular line to any turn?
-    // Filter turns to those matching Dir.turn()
-    // Then filter for any whose y == y (if point.dir == S|N) or x == x (if p.dir == W|E)
-    let pd = point.2.turn();
-    turns
-        .iter()
-        .any(|(x, y, d)| {
-            *d == pd
-                && match point.2 {
-                    Dir::S | Dir::N => *y == point.1,
-                    Dir::E | Dir::W => *x == point.0,
-                }
-        })
-        .then_some(match point.2 {
-            Dir::N => (point.0, point.1 - 1),
-            Dir::S => (point.0, point.1 + 1),
-            Dir::E => (point.0 + 1, point.1),
-            Dir::W => (point.0 - 1, point.1),
-        })
 }
 
 fn walk(c1: &(usize, usize, Dir), c2: &(usize, usize, Dir)) -> Vec<(usize, usize, Dir)> {
@@ -282,40 +218,4 @@ fn find_lowest_gt(px: usize, v: &[usize]) -> Option<usize> {
 
 fn find_highest_lt(px: usize, v: &[usize]) -> Option<usize> {
     v.iter().filter(|i| **i < px).max().map(|i| i + 1)
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn case1() {
-        let input = "....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#..."
-            .to_string();
-
-        let solve = Solve::try_from(input).unwrap();
-        assert_eq!(
-            solve
-                .p1()
-                .map(|v| format!("{}", v).parse::<usize>().unwrap())
-                .unwrap(),
-            41
-        );
-        assert_eq!(
-            solve
-                .p2()
-                .map(|v| format!("{}", v).parse::<usize>().unwrap())
-                .unwrap(),
-            6
-        );
-    }
 }
