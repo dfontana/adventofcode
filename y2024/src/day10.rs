c220cda6bea340b2a74307bb94de4961aa31d0f0
Dylan Fontana <dyfontana@gmail.com>
2024-12-10 12:17:02 -0500

    p2

--- y2024/src/day10.rs
+++ y2024/src/day10.rs
@@ -31,7 +31,12 @@ impl Day for Solve {
     }
 
     fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-        Ok(Box::new(1))
+        Ok(Box::new(
+            get_trail_heads(&self.map)
+                .iter()
+                .map(|loc| unique_trails_from(loc, &self.map).len())
+                .sum::<usize>(),
+        ))
     }
 }
 
@@ -63,6 +68,37 @@ fn reachable_peaks_from(loc: &(usize, usize, &u32), map: &Grid<u32>) -> HashSet<
     ret
 }
 
+fn unique_trails_from(
+    loc: &(usize, usize, &u32),
+    map: &Grid<u32>,
+) -> HashSet<Vec<(usize, usize, u32)>> {
+    // BFS from loc, find all the unique loc's with height == 9
+    let mut ret = HashSet::new();
+    let mut frontier = VecDeque::from_iter(vec![vec![(loc.0, loc.1, *loc.2)]]);
+    while let Some(path) = frontier.pop_front() {
+        let (y, x, h) = path.last().unwrap(); // Never empty
+        for dir in [Dir::E, Dir::W, Dir::N, Dir::S] {
+            if let Some((loc, h1)) = map.at_step(*y, *x, 1, dir) {
+                if *h1 != h + 1 {
+                    // not a valid step to take
+                    continue;
+                }
+                let mut new_path = path.clone();
+                new_path.push((loc.0, loc.1, *h1));
+                if *h1 == 9 {
+                    // Goal found, push to results
+                    ret.insert(new_path);
+                    continue;
+                } else {
+                    // Otherwise we can step this way on our hike
+                    frontier.push_back(new_path);
+                }
+            }
+        }
+    }
+    ret
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -81,11 +117,11 @@ mod test {
         let solve = Solve::try_from(input).unwrap();
         assert_eq!(
             format!("{}", solve.p1().unwrap()).parse::<i64>().unwrap(),
-            1
+            36
         );
         assert_eq!(
             format!("{}", solve.p2().unwrap()).parse::<i64>().unwrap(),
-            1
+            81
         );
     }
 }
