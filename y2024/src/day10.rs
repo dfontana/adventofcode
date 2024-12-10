6a548de29acc8a11809f05f4564fcca526b0741f
Dylan Fontana <dyfontana@gmail.com>
2024-12-10 12:22:43 -0500

    Cleanup

--- y2024/src/day10.rs
+++ y2024/src/day10.rs
@@ -9,63 +9,41 @@ use std::{
 };
 
 pub struct Solve {
-    map: Grid<u32>,
+    peaks: usize,
+    trails: usize,
 }
 impl TryFrom<String> for Solve {
     type Error = Box<dyn Error>;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        Ok(Solve {
-            map: Grid::new_from_map(value, |c| c.to_digit(10).unwrap()),
-        })
+        let map = Grid::new_from_map(value, |c| c.to_digit(10).unwrap());
+        let (peaks, trails) = get_trail_heads(&map)
+            .iter()
+            .map(|loc| unique_trails_from(loc, &map))
+            .fold((0, 0), |(mut peaks, mut trails), head_trails| {
+                trails += head_trails.len();
+                peaks += head_trails
+                    .iter()
+                    .filter_map(|t| t.last())
+                    .collect::<HashSet<_>>()
+                    .len();
+                (peaks, trails)
+            });
+        Ok(Solve { peaks, trails })
     }
 }
 impl Day for Solve {
     fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-        Ok(Box::new(
-            get_trail_heads(&self.map)
-                .iter()
-                .map(|loc| reachable_peaks_from(loc, &self.map).len())
-                .sum::<usize>(),
-        ))
+        Ok(Box::new(self.peaks))
     }
 
     fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-        Ok(Box::new(
-            get_trail_heads(&self.map)
-                .iter()
-                .map(|loc| unique_trails_from(loc, &self.map).len())
-                .sum::<usize>(),
-        ))
+        Ok(Box::new(self.trails))
     }
 }
 
 fn get_trail_heads(map: &Grid<u32>) -> Vec<(usize, usize, &u32)> {
     map.iter().filter(|(_, _, height)| **height == 0).collect()
-}
-
-fn reachable_peaks_from(loc: &(usize, usize, &u32), map: &Grid<u32>) -> HashSet<(usize, usize)> {
-    // BFS from loc, find all the unique loc's with height == 9
-    let mut ret = HashSet::new();
-    let mut frontier = VecDeque::from_iter(vec![(loc.0, loc.1, *loc.2)]);
-    while let Some((y, x, h)) = frontier.pop_front() {
-        for dir in [Dir::E, Dir::W, Dir::N, Dir::S] {
-            if let Some((loc, h1)) = map.at_step(y, x, 1, dir) {
-                if *h1 != h + 1 {
-                    // not a valid step to take
-                    continue;
-                }
-                if *h1 == 9 {
-                    // Goal found, push to results
-                    ret.insert(loc);
-                    continue;
-                }
-                // Otherwise we can step this way on our hike
-                frontier.push_back((loc.0, loc.1, *h1));
-            }
-        }
-    }
-    ret
 }
 
 fn unique_trails_from(
@@ -97,31 +75,4 @@ fn unique_trails_from(
         }
     }
     ret
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn example() {
-        let input = "89010123
-78121874
-87430965
-96549874
-45678903
-32019012
-01329801
-10456732"
-            .to_string();
-        let solve = Solve::try_from(input).unwrap();
-        assert_eq!(
-            format!("{}", solve.p1().unwrap()).parse::<i64>().unwrap(),
-            36
-        );
-        assert_eq!(
-            format!("{}", solve.p2().unwrap()).parse::<i64>().unwrap(),
-            81
-        );
-    }
 }
