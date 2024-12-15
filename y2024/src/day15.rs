b21f9f62456e708cbc5032696b926d551b67ffb2
Dylan Fontana <dyfontana@gmail.com>
2024-12-15 16:25:40 -0500

    idk where the bug is

--- y2024/src/day15.rs
+++ y2024/src/day15.rs
@@ -78,129 +78,92 @@ impl Day for Solve {
 
 fn apply<'a>(start: (usize, usize), grid: &'a mut Grid<char>, insts: &Vec<Dir>) -> &'a Grid<char> {
     let (mut fy, mut fx) = start;
-    // grid.print();
+    grid.print();
     for dir in insts {
-        let mvmt = grid
-            .at_step(fy, fx, 1, dir)
-            .map(|(t, c)| (t, *c))
-            .and_then(|((ny, nx), c)| match c {
-                '.' => Some((ny, nx)),
-                '[' if (*dir == Dir::S || *dir == Dir::N) && prop_move(grid, dir, ny, nx, true) => {
-                    // Clear the tile to the right of the bot
-                    grid.put(ny, fx + 1, '.');
-                    Some((ny, nx))
-                }
-                ']' if (*dir == Dir::S || *dir == Dir::N) && prop_move(grid, dir, ny, nx, true) => {
-                    // Clear the tile to the left of the bot
-                    grid.put(ny, fx - 1, '.');
-                    Some((ny, nx))
-                }
-                '[' | ']' if prop_move(grid, dir, ny, nx, true) => Some((ny, nx)),
-                'O' if prop_move(grid, dir, ny, nx, false) => Some((ny, nx)),
-                _ => None,
+        let mvmt = prop_move(grid, dir, ((fy, fx), *grid.at(fy, fx).unwrap()));
+        if let Some(moves) = mvmt {
+            // println!("Moving {fy},{fx} {dir:?} -> {moves:?}");
+            let mut touched = HashSet::new();
+            moves.iter().for_each(|(_, to, set_c)| {
+                touched.insert(to);
+                grid.put(to.0, to.1, *set_c);
             });
-        if let Some((ny, nx)) = mvmt {
-            grid.put(fy, fx, '.');
+            moves.iter().for_each(|(from, _, _)| {
+                if touched.contains(from) {
+                    return;
+                }
+                grid.put(from.0, from.1, '.');
+            });
+
+            let (_, (ny, nx), to_c) = *moves.last().unwrap();
+            if to_c != '@' {
+                panic!("Order lost");
+            }
             fy = ny;
             fx = nx;
-            grid.put(fy, fx, '@');
         }
         // grid.print();
     }
-    // grid.print();
+    grid.print();
     grid
 }
 
-fn prop_move(grid: &mut Grid<char>, dir: &Dir, fy: usize, fx: usize, double_wide: bool) -> bool {
-    // Need to scan ahead of the boxes, if free spot in dir then move all of them in dir.
-    let mut moves = Vec::new();
-    let has_space = 'sp: {
-        let mut sy = fy;
-        let mut sx = fx;
-        let mut also_check_columns: HashSet<usize> = HashSet::from_iter(vec![sx]);
-        let mut all_free_check: HashSet<usize> = HashSet::new();
-        while let Some(((ny, nx), _)) = grid.at_step(sy, sx, 1, dir) {
-            let mut n_cols: HashSet<usize> = HashSet::new();
-            let mut hit_free = false;
-            for ax in also_check_columns.iter() {
-                let Some(bot_c) = grid.at(sy, *ax) else {
-                    break 'sp false;
-                };
-                // println!("Checking: ({sy},{ax}) -> ({ny},{ax}) {bot_c}");
-                match bot_c {
-                    '#' => {
-                        break 'sp false;
-                    }
-                    '.' if (!double_wide || *dir == Dir::W || *dir == Dir::E) => {
-                        // moves.push(((sy, sx), (ny, nx)));
-                        break 'sp true;
-                    }
-                    'O' => {
-                        moves.push(((sy, sx), (ny, nx)));
-                    }
-                    // Double wide mode has additional behaviors
-                    '[' | ']' if *dir == Dir::W || *dir == Dir::E => {
-                        // This acts like single wide b/c it's not double height.
-                        moves.push(((sy, sx), (ny, nx)));
-                    }
-                    // But if we hit a box edge N/S, we need to start looking for it's other edge
-                    // This can create a cascade to look at
-                    ']' if *dir == Dir::S || *dir == Dir::N => {
-                        moves.push(((sy, *ax), (ny, *ax)));
-                        if !n_cols.contains(&(ax - 1)) {
-                            n_cols.insert(ax - 1);
-                            moves.push(((sy, ax - 1), (ny, ax - 1)));
-                        }
-                    }
-                    '[' if *dir == Dir::S || *dir == Dir::N => {
-                        moves.push(((sy, *ax), (ny, *ax)));
-                        if !n_cols.contains(&(ax + 1)) {
-                            n_cols.insert(ax + 1);
-                            moves.push(((sy, ax + 1), (ny, ax + 1)));
-                        }
-                    }
-                    '.' if double_wide && (*dir == Dir::S || *dir == Dir::N) => {
-                        // All items need ot be free to return true, otherwise it's false
-                        all_free_check.insert(*ax);
-                        moves.push(((sy, *ax), (ny, *ax)));
-                        hit_free = true;
-                    }
-                    x => unreachable!("Ran into char: {}", x),
-                }
-                // println!("\t moves after: {moves:?}");
-            }
-            if hit_free {
-                break 'sp all_free_check == also_check_columns;
-            }
-            also_check_columns.extend(n_cols);
-            // println!("{:?}", also_check_columns);
-            if *dir == Dir::W || *dir == Dir::E {
-                also_check_columns.remove(&sx);
-                also_check_columns.insert(nx);
-            }
-            sy = ny;
-            sx = nx;
-        }
-        false
-    };
-
-    if has_space {
-        // Apply mutations
-        moves.iter().rev().for_each(|(p1, p2)| {
-            let p1s = match grid.at(p1.0, p1.1) {
-                Some(x) => x,
-                None => {
-                    // grid.print();
-                    panic!(
-                    "Tried to move out of the grid going {:?} from {:?} -> {:?} (originally {:?})",
-                    dir, p1, p2, (fy, fx)
-                );
-                }
-            };
-            grid.put(p2.0, p2.1, *p1s);
-        });
-        true
+type Loc = (usize, usize);
+type Move = (Loc, Loc, char);
+fn prop_move(grid: &Grid<char>, dir: &Dir, mf: (Loc, char)) -> Option<Vec<Move>> {
+    if *dir == Dir::E || *dir == Dir::W {
+        rec_move_ew(grid, dir, mf)
     } else {
-        false
+        rec_move_ns(grid, dir, mf)
+    }
+}
+fn rec_move_ew(grid: &Grid<char>, dir: &Dir, (from, from_c): (Loc, char)) -> Option<Vec<Move>> {
+    let (to_loc, to_c) = grid.at_step(from.0, from.1, 1, dir)?;
+    if *to_c == '.' {
+        return Some(vec![(from, to_loc, from_c)]);
+    }
+    if *to_c == '#' {
+        return None;
+    }
+    let mut moves = rec_move_ew(grid, dir, (to_loc, *to_c))?;
+    // If that can move, this can move
+    moves.push((from, to_loc, from_c));
+    Some(moves)
+}
+
+fn rec_move_ns(grid: &Grid<char>, dir: &Dir, (from, moving): (Loc, char)) -> Option<Vec<Move>> {
+    match moving {
+        '[' | ']' => {
+            let dx: isize = if moving == '[' { 1 } else { -1 };
+            let opp = if moving == '[' { ']' } else { '[' };
+            let rx = from.1.checked_add_signed(dx)?;
+            let (tl, lc) = grid.at_step(from.0, from.1, 1, dir)?;
+            let (tr, rc) = grid.at_step(from.0, rx, 1, dir)?;
+            if *lc == '.' && *rc == '.' {
+                return Some(vec![(from, tl, moving), ((from.0, rx), tr, opp)]);
+            }
+            if *lc == '#' || *rc == '#' {
+                return None;
+            }
+            let mut left_moves = rec_move_ns(grid, dir, (tl, *lc))?;
+            let right_moves = rec_move_ns(grid, dir, (tr, *rc))?;
+            left_moves.push((from, tl, moving));
+            left_moves.push(((from.0, rx), tr, opp));
+            left_moves.extend(right_moves);
+            Some(left_moves)
+        }
+        'O' | '@' => {
+            let (to_loc, to_c) = grid.at_step(from.0, from.1, 1, dir)?;
+            if *to_c == '.' {
+                return Some(vec![(from, to_loc, moving)]);
+            }
+            if *to_c == '#' {
+                return None;
+            }
+            let mut moves = rec_move_ns(grid, dir, (to_loc, *to_c))?;
+            moves.push((from, to_loc, moving));
+            Some(moves)
+        }
+        _ => None,
     }
 }
