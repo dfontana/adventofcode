40262c3d4c574f3d509f45d46a4e822488f3b412
Dylan Fontana <dyfontana@gmail.com>
2024-12-20 12:40:59 -0500

    19 P2

--- y2024/src/day19.rs
+++ y2024/src/day19.rs
@@ -1,5 +1,5 @@
 use rust_util::Day;
-use std::{error::Error, fmt::Display};
+use std::{collections::HashMap, error::Error, fmt::Display};
 
 type TType = String;
 type Arrangement = String;
@@ -37,7 +37,13 @@ impl Day for Solve {
     }
 
     fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
-        Ok(Box::new(1))
+        let mut memo = HashMap::new();
+        Ok(Box::new(
+            self.arrangements
+                .iter()
+                .map(|a| count_possible(a, &mut memo, &self.types))
+                .sum::<usize>(),
+        ))
     }
 }
 
@@ -46,12 +52,31 @@ fn is_possible(a: &str, types: &Vec<TType>) -> bool {
         return true;
     }
     for t in types {
-        if a == t {
-            return true;
-        }
         if a.starts_with(t) && is_possible(&a[t.len()..], types) {
             return true;
         }
     }
     false
+}
+
+fn count_possible<'a>(a: &'a str, memo: &mut HashMap<&'a str, usize>, types: &Vec<TType>) -> usize {
+    if a.len() == 0 {
+        return 1;
+    }
+    let mut total_subbranches = 0;
+    for t in types {
+        if a.starts_with(t) {
+            let key = &a[t.len()..];
+            let sub_count = match memo.get(key) {
+                Some(v) => *v,
+                None => {
+                    let sub_count = count_possible(key, memo, types);
+                    memo.insert(key, sub_count);
+                    sub_count
+                }
+            };
+            total_subbranches += sub_count;
+        }
+    }
+    total_subbranches
 }
