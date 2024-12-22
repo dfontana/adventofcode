a1580b47e2599e0cb483104721bdfae0a9450da2
Dylan Fontana <dyfontana@gmail.com>
2024-12-22 12:43:39 -0500

    Halve execution time from allocs

--- y2024/src/day22.rs
+++ y2024/src/day22.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use rust_util::Day;
 use std::{collections::HashMap, error::Error, fmt::Display};
 
@@ -20,33 +19,29 @@ impl TryFrom<String> for Solve {
 impl Day for Solve {
     fn p1(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
         Ok(Box::new(
-            self.snums.iter().map(|n| rotate(*n, 2000)).sum::<i64>(),
+            self.snums
+                .iter()
+                .map(|n| (0..2000).fold(*n, |acc, _| secret(acc)))
+                .sum::<i64>(),
         ))
     }
 
     fn p2(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
         Ok(Box::new(most_banana(
-            &self
-                .snums
-                .iter()
-                .map(|n| deltas(&prices(*n, 2001)))
-                .collect(),
+            &self.snums.iter().map(|n| price_deltas(*n, 2000)).collect(),
         )))
     }
 }
 
-fn rotate(num: i64, times: usize) -> i64 {
-    (0..times).fold(num, |acc, _| secret(acc))
-}
-
-fn most_banana(deltas: &Vec<Vec<(i64, i64)>>) -> i64 {
+type Sequence = [i64; 4];
+fn most_banana(deltas: &Vec<HashMap<Sequence, i64>>) -> i64 {
     // Naively: We could just track the total earned from each sequence encountered.
     // Worst case that means we're storing all unique permutations of (-9, 9) where elements
     // can repeat: 19^4 = 130,321 entries of i64, which fits in memory.
     let mut seq_yield = HashMap::new();
 
     for delta in deltas {
-        for (seq, amt) in first_yields(delta).iter() {
+        for (seq, amt) in delta.iter() {
             seq_yield
                 .entry(*seq)
                 .and_modify(|e| *e += *amt)
@@ -57,31 +52,30 @@ fn most_banana(deltas: &Vec<Vec<(i64, i64)>>) -> i64 {
     *seq_yield.values().max().expect("Deltas must exist")
 }
 
-type Sequence = (i64, i64, i64, i64);
-fn first_yields(delta: &Vec<(i64, i64)>) -> HashMap<Sequence, i64> {
-    let mut seq_yield = HashMap::new();
-    for (d1, d2, d3, d4) in delta.iter().tuple_windows() {
-        seq_yield.entry((d1.1, d2.1, d3.1, d4.1)).or_insert(d4.0);
+fn price_deltas(num: i64, times: usize) -> HashMap<Sequence, i64> {
+    let mut num = num;
+    let mut p_price = num % 10;
+    let mut seq_yield = HashMap::with_capacity(times);
+    let mut k = [0; 4];
+    for i in 0..times {
+        num = secret(num);
+        let price = num % 10;
+        let change = price - p_price;
+        if i < 3 {
+            k[i] = change;
+        } else if i == 3 {
+            k[i] = change;
+            seq_yield.entry(k).or_insert(price);
+        } else {
+            k[0] = k[1];
+            k[1] = k[2];
+            k[2] = k[3];
+            k[3] = change;
+            seq_yield.entry(k).or_insert(price);
+        }
+        p_price = price;
     }
     seq_yield
-}
-
-fn prices(num: i64, times: usize) -> Vec<i64> {
-    let mut num = num;
-    let mut ret = Vec::with_capacity(times);
-    for _ in 0..times {
-        ret.push(num % 10);
-        num = secret(num);
-    }
-    ret
-}
-
-fn deltas(nums: &Vec<i64>) -> Vec<(i64, i64)> {
-    let mut ret = Vec::with_capacity(nums.len());
-    for i in 1..nums.len() {
-        ret.push((nums[i], nums[i] - nums[i - 1]))
-    }
-    ret
 }
 
 fn secret(num: i64) -> i64 {
